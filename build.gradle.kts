7d1c10eb555e131ace472cf7dc35738364e3afa8
Craig Miller <craigmiller160@gmail.com>
2023-03-08 16:21:43 -0500

    Heavily refactored gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val projectGroup: String by project
+val projectVersion: String by project
+
 plugins {
     id("org.springframework.boot") version "3.0.4"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("jvm")
+    kotlin("plugin.spring")
 }
 
-group = "io.craigmiller160"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
-
-repositories {
-    mavenCentral()
-}
+group = projectGroup
+version = projectVersion
+java.sourceCompatibility = JavaVersion.VERSION_19
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
@@ -34,7 +33,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "19"
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
-rootProject.name = "market-tracker-profile-service"
+val projectName: String by settings
+
+rootProject.name = projectName
+
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+    }
+}
