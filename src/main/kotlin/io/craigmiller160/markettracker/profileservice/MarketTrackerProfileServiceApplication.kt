2fd173ae7d9cdd8cd0a56b14c6c8da80dc3a1385
Craig Miller <craigmiller160@gmail.com>
2023-03-08 17:14:33 -0500

    Formatted files

--- src/main/kotlin/io/craigmiller160/markettracker/profileservice/MarketTrackerProfileServiceApplication.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/profileservice/MarketTrackerProfileServiceApplication.kt
@@ -3,9 +3,8 @@ package io.craigmiller160.markettracker.profileservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class MarketTrackerProfileServiceApplication
+@SpringBootApplication class MarketTrackerProfileServiceApplication
 
 fun main(args: Array<String>) {
-    runApplication<MarketTrackerProfileServiceApplication>(*args)
+  runApplication<MarketTrackerProfileServiceApplication>(*args)
 }
--- src/test/kotlin/io/craigmiller160/markettracker/profileservice/MarketTrackerProfileServiceApplicationTests.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/profileservice/MarketTrackerProfileServiceApplicationTests.kt
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class MarketTrackerProfileServiceApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
-
+  @Test fun contextLoads() {}
 }
