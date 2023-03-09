b28d19b7d3d846091bdc096226931fb74eadf681
Craig Miller <craigmiller160@gmail.com>
2023-03-09 15:41:37 -0500

    Implemented converter

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/config/JwtAuthConverterConfig.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/config/JwtAuthConverterConfig.kt
@@ -3,4 +3,4 @@ package io.craigmiller160.markettracker.portfolio.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "jwt.auth.converter")
-data class JwtAuthConverterConfig(val resourceId: String, val principalAttribute: String)
+data class JwtAuthConverterConfig(val resourceId: String, val principalAttribute: String? = null)
