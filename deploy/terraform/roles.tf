23b21b393721863ffb5c673c27481a56f26da57d
Craig Miller <craigmiller160@gmail.com>
2023-03-09 14:21:20 -0500

    Even more of a roles refactor

--- deploy/terraform/roles.tf
+++ deploy/terraform/roles.tf
@@ -16,13 +16,13 @@ data "keycloak_openid_client" "market_tracker_api_prod" {
 
 data "keycloak_role" "market_tracker_api_access_role_dev" {
   realm_id  = data.keycloak_realm.apps_dev.id
-  client_id = data.keycloak_openid_client.market_tracker_api_dev
+  client_id = data.keycloak_openid_client.market_tracker_api_dev.id
   name = local.access_role_common.name
 }
 
 data "keycloak_role" "market_tracker_api_access_role_prod" {
   realm_id  = data.keycloak_realm.apps_prod.id
-  client_id = data.keycloak_openid_client.market_tracker_api_prod
+  client_id = data.keycloak_openid_client.market_tracker_api_prod.id
   name = local.access_role_common.name
 }
 
@@ -39,18 +39,20 @@ resource "keycloak_role" "market_tracker_portfolio_service_access_role_prod" {
 }
 
 
-#resource "keycloak_role" "admin_role" {
-#  realm_id        = keycloak_realm.realm.id
-#  name            = "admin"
-#  composite_roles = [
-#    keycloak_role.create_role.id,
-#    keycloak_role.read_role.id,
-#    keycloak_role.update_role.id,
-#    keycloak_role.delete_role.id,
-#    keycloak_role.client_role.id,
-#  ]
-#
-#  attributes = {
-#    key = "value"
-#  }
-#}
+resource "keycloak_role" "market_tracker_all_access_dev" {
+  realm_id = data.keycloak_realm.apps_dev.id
+  name = "market_tracker_all_access"
+  composite_roles = [
+    data.keycloak_role.market_tracker_api_access_role_dev,
+    keycloak_role.market_tracker_portfolio_service_access_role_dev
+  ]
+}
+
+resource "keycloak_role" "market_tracker_all_access_dev" {
+  realm_id = data.keycloak_realm.apps_prod.id
+  name = "market_tracker_all_access"
+  composite_roles = [
+    data.keycloak_role.market_tracker_api_access_role_prod,
+    keycloak_role.market_tracker_portfolio_service_access_role_prod
+  ]
+}
