dabf8506b804dbba264580647be96b72e44070c7
Craig Miller <craigmiller160@gmail.com>
2023-03-10 14:53:53 -0500

    More progress with test setup

--- src/test/kotlin/io/craigmiller160/markettracker/profileservice/web/controllers/PracticeController2Test.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/profileservice/web/controllers/PracticeController2Test.kt
@@ -2,12 +2,15 @@ package io.craigmiller160.markettracker.profileservice.web.controllers
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
-class PracticeController2Test {
+@AutoConfigureMockMvc
+class PracticeController2Test(private val mockMvc: MockMvc) {
   @Test
   fun practice() {
     TODO()
