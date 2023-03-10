c057b91b41fd9176361daf18e2b2d4df9ae30930
Craig Miller <craigmiller160@gmail.com>
2023-03-10 15:27:40 -0500

    Got the r2dbc incompatibility with testcontainer lib resolved

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2Test.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2Test.kt
@@ -4,7 +4,6 @@ import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
@@ -16,15 +15,9 @@ import org.springframework.test.web.servlet.get
 @ExtendWith(TestcontainersExtension::class, SpringExtension::class)
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
-class PracticeController2Test
-@Autowired
-constructor(
-    private val webTestClient: WebTestClient,
-    @Value("\${spring.r2dbc.url}") private val prop: String
-) {
+class PracticeController2Test @Autowired constructor(private val webTestClient: WebTestClient) {
   @Test
   fun practice() {
-    println(prop) // TODO delete this
     webTestClient
         .get()
         .uri("/practice2")
