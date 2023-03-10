ed07ceb0fba344a2a55a03973ab9cf7b552e8971
Craig Miller <craigmiller160@gmail.com>
2023-03-10 15:10:39 -0500

    Working on integrating testcontainer properties

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2Test.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2Test.kt
@@ -1,5 +1,6 @@
 package io.craigmiller160.markettracker.portfolio.web.controllers
 
+import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,7 +11,7 @@ import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.servlet.get
 
 @SpringBootTest
-@ExtendWith(SpringExtension::class)
+@ExtendWith(TestcontainersExtension::class, SpringExtension::class)
 @AutoConfigureWebTestClient
 class PracticeController2Test @Autowired constructor(private val webTestClient: WebTestClient) {
   @Test
