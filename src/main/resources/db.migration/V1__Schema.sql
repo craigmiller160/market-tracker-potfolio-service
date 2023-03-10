50d0fac1ae5b366b71a65cef53612843c8eba1e3
Craig Miller <craigmiller160@gmail.com>
2023-03-10 15:44:35 -0500

    Working on raw share data

--- src/main/resources/db.migration/V1__Schema.sql
+++ src/main/resources/db.migration/V1__Schema.sql
@@ -1,1 +1,10 @@
 CREATE EXTENSION pgcrypto;
+
+CREATE TABLE raw_share_data (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    date DATE NOT NULL,
+    symbol VARCHAR(10) NOT NULL,
+    total_shares DECIMAL NOT NULL,
+    PRIMARY KEY (id)
+);
