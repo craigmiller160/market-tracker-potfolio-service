27121fa2e18f78350c347b5b6deeca9efc5e75d5
Craig Miller <craigmiller160@gmail.com>
2023-03-10 14:51:34 -0500

    Preparing flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.security:spring-security-test")
+//    implementation("org.flywaydb:flyway-core")
 }
 
 tasks.withType<KotlinCompile> {
