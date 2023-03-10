8dc9ba487e3e729da43d9278ccc4b04a07c20ab8
Craig Miller <craigmiller160@gmail.com>
2023-03-10 14:52:47 -0500

    Stubbed out test

--- src/test/kotlin/io/craigmiller160/markettracker/profileservice/web/controllers/PracticeController2Test.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/profileservice/web/controllers/PracticeController2Test.kt
@@ -1,7 +1,15 @@
 package io.craigmiller160.markettracker.profileservice.web.controllers
 
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-@SpringBootTest @ExtendWith(SpringExtension::class) class PracticeController2Test {}
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class PracticeController2Test {
+  @Test
+  fun practice() {
+    TODO()
+  }
+}
