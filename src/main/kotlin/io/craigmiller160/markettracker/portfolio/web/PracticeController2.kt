b39d341cd8be759dda98b99e565dd0a7cd20b276
Craig Miller <craigmiller160@gmail.com>
2023-03-10 14:46:31 -0500

    Leaning towards a controller-based solution due to swagger support

---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/PracticeController2.kt
@@ -0,0 +1,12 @@
+package io.craigmiller160.markettracker.portfolio.web
+
+import io.craigmiller160.markettracker.portfolio.web.types.PracticeResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/practice2")
+class PracticeController2 {
+  @GetMapping suspend fun hello(): PracticeResponse = PracticeResponse("Hello World 2")
+}
--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoute.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoute.kt
@@ -1,11 +1,16 @@
 package io.craigmiller160.markettracker.portfolio.web.routes
 
 import io.craigmiller160.markettracker.portfolio.web.handlers.PracticeHandler
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.core.annotations.RouterOperations
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.reactive.function.server.coRouter
 
 @Component
 class PracticeRoute {
-  @Bean fun practice(handler: PracticeHandler) = coRouter { GET("/practice", handler::practice) }
+  @Bean
+  @RouterOperations(value = [RouterOperation(path = "/practice", method = [RequestMethod.GET])])
+  fun practice(handler: PracticeHandler) = coRouter { GET("/practice", handler::practice) }
 }
