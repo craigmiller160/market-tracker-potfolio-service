5fc07c242df229b5c2102a9e2122dc66a2a26299
Craig Miller <craigmiller160@gmail.com>
2023-03-10 15:30:36 -0500

    Configured default users

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/testutils/AuthenticationHelperConfig.kt
@@ -0,0 +1,22 @@
+package io.craigmiller160.markettracker.portfolio.testutils
+
+import io.craigmiller160.testcontainers.common.core.AuthenticationHelper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AuthenticationHelperConfig {
+  @Bean
+  fun authenticationHelper(): AuthenticationHelper {
+    return AuthenticationHelper()
+  }
+
+  @Bean
+  fun defaultUsers(authHelper: AuthenticationHelper): DefaultUsers {
+    val primaryUser = authHelper.createUser("primary@gmail.com").let { authHelper.login(it) }
+    val secondaryUser = authHelper.createUser("secondary@gmail.com").let { authHelper.login(it) }
+    val tertiaryUser = authHelper.createUser("tertiary@gmail.com").let { authHelper.login(it) }
+    return DefaultUsers(
+        primaryUser = primaryUser, secondaryUser = secondaryUser, tertiaryUser = tertiaryUser)
+  }
+}
