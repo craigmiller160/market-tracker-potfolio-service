9278fa4d2099fa8cfc28a9bac233e965d4bab966
Craig Miller <craigmiller160@gmail.com>
2023-03-11 12:19:20 -0500

    More work on dsl

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/SwaggerRouterDsl.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/SwaggerRouterDsl.kt
@@ -7,13 +7,24 @@ import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 
+typealias SwaggerOperationBuilder = org.springdoc.core.fn.builders.operation.Builder
+
 fun suspendToMonoHandler(
     handler: suspend (ServerRequest) -> ServerResponse
 ): (ServerRequest) -> Mono<ServerResponse> = { request -> mono { handler(request) } }
 
+class SwaggerDsl(private val builder: SwaggerOperationBuilder) {}
+
 class SwaggerRouterDsl(private val builder: SpringdocRouteBuilder) {
-  fun GET(path: String, handler: suspend (ServerRequest) -> ServerResponse) {
-    builder.GET(path, suspendToMonoHandler(handler)) {}
+  fun GET(
+      path: String,
+      handler: suspend (ServerRequest) -> ServerResponse,
+      swagger: SwaggerDsl.() -> Unit = {}
+  ) {
+    builder.GET(path, suspendToMonoHandler(handler)) { builder ->
+      val swaggerDsl = SwaggerDsl(builder)
+      swaggerDsl.swagger()
+    }
   }
 }
 
