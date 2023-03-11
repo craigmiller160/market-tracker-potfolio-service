9792211e564fefd7f71b039013efb9186bcc4571
Craig Miller <craigmiller160@gmail.com>
2023-03-11 15:26:18 -0500

    Progress

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/JwtGenerator.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/JwtGenerator.kt
@@ -2,13 +2,18 @@ package io.craigmiller160.markettracker.portfolio
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.nimbusds.jose.crypto.RSASSASigner
 import com.nimbusds.jwt.JWTClaimsSet
 import java.nio.file.Paths
+import java.security.KeyFactory
+import java.security.spec.PKCS8EncodedKeySpec
 import java.time.ZoneId
 import java.time.ZonedDateTime
+import java.util.Base64
 import kotlin.io.path.readText
 
 fun main() {
+  // https://developers.google.com/identity/protocols/oauth2/service-account#httprest
   val accountPath =
       Paths.get(
           System.getProperty("user.home"), "Downloads", "market-tracker-380317-fdda285edeac.json")
@@ -29,6 +34,17 @@ fun main() {
           "exp" to nowUtc.plusMinutes(30).toEpochSecond())
 
   val claimsSet = JWTClaimsSet.parse(claims)
+
+  val privateKeyBytes =
+      account.privateKey
+          .replace("-----BEGIN PRIVATE KEY-----", "")
+          .replace("-----END PRIVATE KEY-----", "")
+          .replace(Regex("\\s+"), "")
+          .let { Base64.getDecoder().decode(it) }
+  val keySpec = PKCS8EncodedKeySpec(privateKeyBytes)
+  val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpec)
+
+  val signer = RSASSASigner(privateKey)
 }
 
 data class Account(
