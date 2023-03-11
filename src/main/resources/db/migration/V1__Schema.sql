8ced4eccd482159d889aab24fbb799a14270b2bf
Craig Miller <craigmiller160@gmail.com>
2023-03-11 10:46:15 -0500

    Cleanup TODO

--- src/main/resources/db/migration/V1__Schema.sql
+++ src/main/resources/db/migration/V1__Schema.sql
@@ -14,13 +14,12 @@ CREATE TABLE shares_owned (
     id UUID NOT NULL,
     user_id UUID NOT NULL,
     portfolio_id UUID NOT NULL,
-    -- TODO need an index, does the GIST auto-apply an index? I don't think so
     date_range DATERANGE NOT NULL,
     symbol VARCHAR(10) NOT NULL,
     total_shares DECIMAL NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (portfolio_id) REFERENCES portfolios (id),
-    EXCLUDE USING GIST (user_id WITH =, portfolio_id WITH =, date WITH &&)
+    EXCLUDE USING GIST (user_id WITH =, portfolio_id WITH =, date_range WITH &&)
 );
 CREATE INDEX ON shares_owned (user_id);
 CREATE INDEX ON shares_owned (portfolio_id);
