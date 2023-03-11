cdb6c4dc65130753fe57a2d1cd42e1655f49ae0a
Craig Miller <craigmiller160@gmail.com>
2023-03-11 11:28:43 -0500

    Working on routing and swagger

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/PracticeHandler.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/PracticeHandler.kt
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 @Component
 class PracticeHandler {
   suspend fun helloUniverse(request: ServerRequest): ServerResponse =
       ServerResponse.ok().body(BodyInserters.fromValue("Hello Universe")).awaitSingle()
+
+  fun helloUniverseMono(request: ServerRequest): Mono<ServerResponse> =
+      ServerResponse.ok().body(BodyInserters.fromValue("Hello Universe"))
 }
--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoutesHolder.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoutesHolder.kt
@@ -1,6 +1,8 @@
 package io.craigmiller160.markettracker.portfolio.web.routes
 
 import io.craigmiller160.markettracker.portfolio.web.handlers.PracticeHandler
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.RouterFunction
@@ -10,7 +12,19 @@ import org.springframework.web.reactive.function.server.coRouter
 @Component
 class PracticeRoutesHolder {
   @Bean
+  @RouterOperation(beanClass = PracticeHandler::class, beanMethod = "helloUniverse")
   fun practiceRoutes(handler: PracticeHandler): RouterFunction<ServerResponse> = coRouter {
     GET("/router/practice", handler::helloUniverse)
+  }
+
+  @Bean
+  fun practiceRoutes2(handler: PracticeHandler): RouterFunction<ServerResponse> {
+    return route()
+        .GET("/router/practice2", handler::helloUniverseMono) { ops ->
+          ops.operationId("getPractice2")
+              .beanClass(PracticeHandler::class.java)
+              .beanMethod("helloUniverseMono")
+        }
+        .build()
   }
 }
