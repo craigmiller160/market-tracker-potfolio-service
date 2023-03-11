081dc3b914e907e69614944d16316c72060828b2
Craig Miller <craigmiller160@gmail.com>
2023-03-11 10:11:45 -0500

    More work on schema

--- src/main/resources/db/migration/V1__Schema.sql
+++ src/main/resources/db/migration/V1__Schema.sql
@@ -8,14 +8,17 @@ CREATE TABLE portfolios (
     name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+CREATE INDEX ON portfolios (user_id);
 
 CREATE TABLE shares_owned (
     id UUID NOT NULL,
     user_id UUID NOT NULL,
     portfolio_id UUID NOT NULL,
-    date DATE NOT NULL,
+    date DATERANGE NOT NULL,
     symbol VARCHAR(10) NOT NULL,
     total_shares DECIMAL NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (portfolio_id) REFERENCES portfolios (id)
 );
+CREATE INDEX ON shares_owned (user_id);
+CREATE INDEX ON shares_owned (portfolio_id);
