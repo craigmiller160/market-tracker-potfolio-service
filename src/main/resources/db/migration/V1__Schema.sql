658ec651d521f66936c95aa1d0de6d0eb53cb22c
Craig Miller <craigmiller160@gmail.com>
2023-03-11 09:48:56 -0500

    Tweaking the data model

--- src/main/resources/db/migration/V1__Schema.sql
+++ src/main/resources/db/migration/V1__Schema.sql
@@ -1,10 +1,19 @@
 CREATE EXTENSION pgcrypto;
 
-CREATE TABLE raw_share_data (
+CREATE TABLE portfolios (
     id UUID NOT NULL,
     user_id UUID NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE shares_owned (
+    id UUID NOT NULL,
+    user_id UUID NOT NULL,
+    portfolio_id UUID NOT NULL,
     date DATE NOT NULL,
     symbol VARCHAR(10) NOT NULL,
     total_shares DECIMAL NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (portfolio_id) REFERENCES portfolios (id)
 );
