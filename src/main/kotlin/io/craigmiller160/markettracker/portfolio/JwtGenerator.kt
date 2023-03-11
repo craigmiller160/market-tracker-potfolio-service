19473cbeddbd7b63844b0e08a03971048d78f19b
Craig Miller <craigmiller160@gmail.com>
2023-03-11 14:46:58 -0500

    Fixed error

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/JwtGenerator.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/JwtGenerator.kt
@@ -1,6 +1,6 @@
 package io.craigmiller160.markettracker.portfolio
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.nio.file.Paths
 import kotlin.io.path.readText
@@ -10,7 +10,7 @@ fun main() {
       Paths.get(
           System.getProperty("user.home"), "Downloads", "market-tracker-380317-fdda285edeac.json")
   val objectMapper =
-      jacksonObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+      jacksonObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
   val account = objectMapper.readValue(accountPath.readText(), Account::class.java)
   println(account)
 }
