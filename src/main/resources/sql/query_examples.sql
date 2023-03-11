5fb30cf1cf7a6ec19628cb3f408677485b549f8a
Craig Miller <craigmiller160@gmail.com>
2023-03-11 10:35:49 -0500

    More progress with query examples

--- src/main/resources/sql/query_examples.sql
+++ src/main/resources/sql/query_examples.sql
@@ -16,3 +16,13 @@ where '2023-01-15'::date <@ date;
 -- Generate the date series
 select date_trunc('day', dd)::date
 from generate_series('2023-01-01'::date, '2023-02-01'::date, '1 day'::interval) dd;
+
+-- CTE that generates the date range and then does the query
+with the_dates as (
+    select date_trunc('day', dd)::date as the_date
+    from generate_series('2023-01-01'::date, '2023-02-01'::date, '1 day'::interval) dd
+)
+select td.the_date, so.*
+from the_dates td
+-- Will end after the max date in shares_owned, even if I don't want that
+join shares_owned so on td.the_date <@ so.date;
