1ed0f5953c854d6a942338aef3d7225354416bdb
Craig Miller <craigmiller160@gmail.com>
2023-03-11 12:39:25 -0500

    More progress

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoutesHolder.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PracticeRoutesHolder.kt
@@ -3,53 +3,13 @@ package io.craigmiller160.markettracker.portfolio.web.routes
 import io.craigmiller160.markettracker.portfolio.web.handlers.PracticeHandler
 import io.craigmiller160.markettracker.portfolio.web.swagger.SwaggerBuilders
 import io.craigmiller160.markettracker.portfolio.web.types.PracticeResponse
-import io.swagger.v3.oas.annotations.enums.ParameterIn
-import kotlinx.coroutines.reactor.awaitSingle
-import org.springdoc.core.annotations.RouterOperation
-import org.springdoc.core.annotations.RouterOperations
-import org.springdoc.core.fn.builders.parameter.Builder
-import org.springdoc.webflux.core.fn.SpringdocRouteBuilder.route
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.coRouter
 
 @Component
-class PracticeRoutesHolder(private val handler: PracticeHandler) {
-  @Bean
-  @RouterOperations(
-      value =
-          [
-              RouterOperation(beanClass = PracticeHandler::class, beanMethod = "helloUniverse"),
-              RouterOperation(
-                  beanClass = PracticeHandler::class,
-                  beanMethod = "helloName",
-                  path = "/router/practice3/{name}",
-                  method = [RequestMethod.GET])])
-  fun practiceRoutes(handler: PracticeHandler): RouterFunction<ServerResponse> = coRouter {
-    GET("/router/practice", handler::helloUniverse)
-    GET("/router/practice3/{name}", ::helloName)
-  }
-
-  private suspend fun helloName(request: ServerRequest): ServerResponse =
-      with(request) { request.pathVariable("name") }
-          .let { name -> handler.helloName(name) }
-          .let { ServerResponse.ok().body(BodyInserters.fromValue(it)).awaitSingle() }
-
-  @Bean
-  fun practiceRoutes2(handler: PracticeHandler): RouterFunction<ServerResponse> {
-    return route()
-        .GET("/router/practice2/{param}", handler::helloUniverseMono) { ops ->
-          ops.operationId("getPractice2")
-              .parameter(Builder.parameterBuilder().name("param").`in`(ParameterIn.PATH))
-              .parameter(Builder.parameterBuilder().name("queryParam").`in`(ParameterIn.QUERY))
-        }
-        .build()
-  }
+class PracticeRoutesHolder {
 
   @Bean
   fun practiceRoutes3(handler: PracticeHandler): RouterFunction<ServerResponse> = coSwaggerRouter {
