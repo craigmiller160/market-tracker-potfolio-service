9de3798440a39ebb84435ad724eea63890ea82a1
Craig Miller <craigmiller160@gmail.com>
2023-03-11 19:21:37 -0500

    Setting up more config values for downloaders

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/CraigMillerDownloaderService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/CraigMillerDownloaderService.kt
@@ -3,9 +3,15 @@ package io.craigmiller160.markettracker.portfolio.service.downloaders
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import kotlinx.coroutines.flow.Flow
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 class CraigMillerDownloaderService : DownloaderService {
+  companion object {
+    const val GOOGLE_SHEETS_API_BASE_URL = "https://sheets.googleapis.com/v4"
+  }
+
+  private val webClient = WebClient.create(GOOGLE_SHEETS_API_BASE_URL)
   override suspend fun download(): Flow<SharesOwned> {
     TODO("Not yet implemented")
   }
