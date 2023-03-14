ce84cf70356e58b152461c6cd0ab6d317cd6fda7
Craig Miller <craigmiller160@gmail.com>
2023-03-14 16:32:11 -0400

    Fixed bugs in sql script

--- src/main/resources/db/migration/V1_20230314__Timestamp_Updates.sql
+++ src/main/resources/db/migration/V1_20230314__Timestamp_Updates.sql
@@ -29,12 +29,12 @@ BEFORE INSERT ON shares_owned
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamps_on_insert();
 
-CREATE TRIGGER portfolios_timestamps_on_insert
+CREATE TRIGGER portfolios_timestamps_on_update
 BEFORE UPDATE ON portfolios
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamps_on_update();
 
-CREATE TRIGGER shares_owned_timestamps_on_insert
+CREATE TRIGGER shares_owned_timestamps_on_update
 BEFORE UPDATE ON shares_owned
 FOR EACH ROW
 EXECUTE FUNCTION update_timestamps_on_update();
