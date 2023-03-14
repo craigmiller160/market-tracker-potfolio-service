ed3d2aa079642f611d892429b3ac062bd96ce1fb
Craig Miller <craigmiller160@gmail.com>
2023-03-14 16:30:56 -0400

    Updated sql

--- src/main/resources/db/migration/V1_20230311__Timestamp_Updates.sql
+++ src/main/resources/db/migration/V1_20230311__Timestamp_Updates.sql
@@ -1,3 +1,44 @@
 -- TODO need to automatically set insert/update timestamps
 -- TODO on insert, set both
 -- TODO on update, set only update
+
+CREATE FUNCTION update_timestamps_on_insert()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+    BEGIN
+        NEW.insert_timestamp = now();
+        NEW.update_timestamp = now();
+    END;
+$$;
+
+CREATE FUNCTION update_timestamps_on_update()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+    BEGIN
+        NEW.update_timestamp = now();
+    END;
+$$;
+
+CREATE TRIGGER portfolios_timestamps_on_insert
+BEFORE INSERT ON portfolios
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamps_on_insert();
+
+CREATE TRIGGER shares_owned_timestamps_on_insert
+BEFORE INSERT ON shares_owned
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamps_on_insert();
+
+CREATE TRIGGER portfolios_timestamps_on_insert
+BEFORE UPDATE ON portfolios
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamps_on_update();
+
+CREATE TRIGGER shares_owned_timestamps_on_insert
+BEFORE UPDATE ON shares_owned
+FOR EACH ROW
+EXECUTE FUNCTION update_timestamps_on_update();
