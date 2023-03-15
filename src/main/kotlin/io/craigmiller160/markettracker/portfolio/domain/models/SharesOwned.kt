2951e95b3507aed2710d07f260ae368ab5216774
Craig Miller <craigmiller160@gmail.com>
2023-03-15 07:45:52 -0400

    Updated SharesOwned

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwned.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwned.kt
@@ -4,13 +4,15 @@ import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
 import io.craigmiller160.markettracker.portfolio.common.typedid.SharesOwnedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
+import java.time.LocalDate
 import java.time.ZonedDateTime
 
 data class SharesOwned(
     val id: TypedId<SharesOwnedId>,
     val userId: TypedId<UserId>,
     val portfolioId: TypedId<PortfolioId>,
-    val dateRange: Any, // TODO figure this out
+    val dateRangeStart: LocalDate,
+    val dateRangeEnd: LocalDate,
     val symbol: String,
     val totalShares: Double,
     val insertTimestamp: ZonedDateTime,
