c484f2412becf38fb6bec7209060b8fc32903b33
Craig Miller <craigmiller160@gmail.com>
2023-03-16 16:00:19 -0400

    Working on record transformation logic

---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerTransactionRecord.kt
@@ -0,0 +1,60 @@
+package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
+
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.zip
+import io.craigmiller160.markettracker.portfolio.extensions.KtResult
+import io.craigmiller160.markettracker.portfolio.extensions.ktRunCatching
+import java.lang.IllegalArgumentException
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+enum class Action(val label: String) {
+  BONUS("Bonus"),
+  DEPOSIT("Deposit"),
+  BUY("Buy"),
+  SELL("Sell"),
+  WITHDRAWAL("Withdrawal"),
+  DIVIDEND("Dividend"),
+  CASH_BALANCE("Cash Balance"),
+  INVESTMENT_BALANCE("Investment Balance");
+
+  companion object
+}
+
+fun Action.Companion.fromLabel(label: String): KtResult<Action> =
+    Action.values().find { it.label == label }?.let { Ok(it) }
+        ?: Err(IllegalArgumentException("Invalid label for action: $label"))
+
+data class CraigMillerTransactionRecord(
+    val date: LocalDate,
+    val action: Action,
+    val amount: BigDecimal,
+    val symbol: String,
+    val shares: BigDecimal
+) {
+  companion object
+}
+
+private val transactionDateFormat = DateTimeFormatter.ofPattern("M/d/yyyy")
+
+fun CraigMillerTransactionRecord.Companion.fromRaw(
+    rawRecord: List<String>
+): KtResult<CraigMillerTransactionRecord> {
+  val dateResult = ktRunCatching { LocalDate.parse(rawRecord[0], transactionDateFormat) }
+  val actionResult = Action.fromLabel(rawRecord[1])
+  val amountResult = ktRunCatching {
+    rawRecord[2].replace(Regex("^\\$"), "").let { BigDecimal(it) }
+  }
+  val symbol = rawRecord[3]
+  val sharesResult = ktRunCatching { BigDecimal(rawRecord[4]) }
+  return zip({ dateResult }, { actionResult }, { amountResult }, { sharesResult }) {
+      date,
+      action,
+      amount,
+      shares ->
+    CraigMillerTransactionRecord(
+        date = date, action = action, amount = amount, shares = shares, symbol = symbol)
+  }
+}
