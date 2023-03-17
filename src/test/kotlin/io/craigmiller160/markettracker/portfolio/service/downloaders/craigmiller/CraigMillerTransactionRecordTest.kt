a8aeb05e58fa93285ee6f05dc4eee67030f3a21c
Craig Miller <craigmiller160@gmail.com>
2023-03-16 20:35:19 -0400

    Working on tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerTransactionRecordTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerTransactionRecordTest.kt
@@ -1,11 +1,32 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CraigMillerTransactionRecordTest {
-  @Test
-  fun `Action fromLabel`() {
+  @ParameterizedTest
+  @ValueSource(
+      strings =
+          [
+              "Bonus",
+              "Deposit",
+              "Buy",
+              "Sell",
+              "Withdrawal",
+              "Dividend",
+              "Cash Balance",
+              "Investment Balance"])
+  fun `Action fromLabel, success`(value: String) {
     TODO()
+  }
+
+  @Test
+  fun `Action fromLabel, fail`() {
+    val ex = assertThrows<IllegalArgumentException> { Action.fromLabel("foo") }
+    assertEquals("Invalid label for action: foo", ex.message)
   }
 
   @Test
