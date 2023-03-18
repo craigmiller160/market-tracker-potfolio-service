65ba8ec262731107cc412886c41c56c82b4242e7
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:25:21 -0400

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingServiceTest.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.markettracker.portfolio.service.downloaders
+
+import org.junit.jupiter.api.Test
+
+class DownloaderSchedulingServiceTest {
+  @Test
+  fun `downloads data and writes to database`() {
+    TODO()
+  }
+}
