b2268563f707796b6974cde8d991da1216e3da7d
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:48:08 -0400

    Fixing test setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/MarketTrackerPortfolioIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/MarketTrackerPortfolioIntegrationTest.kt
@@ -3,11 +3,13 @@ package io.craigmiller160.markettracker.portfolio.testcore
 import io.craigmiller160.testcontainers.common.TestcontainersExtension
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
+@SpringBootTest
 @ExtendWith(value = [TestcontainersExtension::class, SpringExtension::class])
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
