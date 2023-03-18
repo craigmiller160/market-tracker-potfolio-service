f8ca89c9d16cd4c0feffc4a767053d39ff60fd4c
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:39:40 -0400

    More test setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingServiceTest.kt
@@ -1,11 +1,8 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
-import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
 import org.junit.jupiter.api.Test
 
 class DownloaderSchedulingServiceTest {
-  private val transactions1: String = DataLoader.load("data/craigmiller/Transactions1.json")
-  private val transactions2: String = DataLoader.load("data/craigmiller/Transactions2.json")
   @Test
   fun `downloads data and writes to database`() {
     TODO()
--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
@@ -1,10 +1,31 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 @MarketTrackerPortfolioIntegrationTest
 class CraigMillerDownloaderServiceTest {
+  companion object {
+    private val transactions1: String = DataLoader.load("data/craigmiller/Transactions1.json")
+    private val transactions2: String = DataLoader.load("data/craigmiller/Transactions2.json")
+    private val mockServer = MockWebServer()
+
+    @BeforeAll
+    @JvmStatic
+    fun beforeAll() {
+      mockServer.start(1234)
+    }
+
+    @AfterAll
+    @JvmStatic
+    fun afterAll() {
+      mockServer.shutdown()
+    }
+  }
   @Test
   fun `downloads and formats google sheet data`() {
     TODO()
