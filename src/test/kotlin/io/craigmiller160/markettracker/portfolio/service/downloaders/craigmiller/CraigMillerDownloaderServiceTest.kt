f6780a45707301de227c3ea42fd88e2bd8908926
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:45:10 -0400

    Test setup logic

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmille
 
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
+import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
@@ -10,10 +11,12 @@ import org.springframework.beans.factory.annotation.Value
 
 @MarketTrackerPortfolioIntegrationTest
 class CraigMillerDownloaderServiceTest(
-    @Value("\${downloaders.craigmiller.test-port}") private val testPort: Int
+    @Value("\${downloaders.craigmiller.test-port}") private val testPort: Int,
+    private val service: CraigMillerDownloaderService
 ) {
   companion object {
     private val transactions1: String = DataLoader.load("data/craigmiller/Transactions1.json")
+    // TODO delete second one if unnecessary
     private val transactions2: String = DataLoader.load("data/craigmiller/Transactions2.json")
   }
 
@@ -31,6 +34,13 @@ class CraigMillerDownloaderServiceTest(
 
   @Test
   fun `downloads and formats google sheet data`() {
+    repeat(3) {
+      mockServer.enqueue(
+          MockResponse().apply {
+            status = "200"
+            setBody(transactions1)
+          })
+    }
     TODO()
   }
 }
