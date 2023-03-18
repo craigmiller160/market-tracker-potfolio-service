bed6d2318366b8a4262d2e74b9cc27e116fcbb26
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:42:08 -0400

    More progress with setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
@@ -3,8 +3,8 @@ package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmille
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 @MarketTrackerPortfolioIntegrationTest
@@ -12,20 +12,21 @@ class CraigMillerDownloaderServiceTest {
   companion object {
     private val transactions1: String = DataLoader.load("data/craigmiller/Transactions1.json")
     private val transactions2: String = DataLoader.load("data/craigmiller/Transactions2.json")
-    private val mockServer = MockWebServer()
-
-    @BeforeAll
-    @JvmStatic
-    fun beforeAll() {
-      mockServer.start(1234)
-    }
-
-    @AfterAll
-    @JvmStatic
-    fun afterAll() {
-      mockServer.shutdown()
-    }
   }
+
+  private val mockServer = MockWebServer()
+
+  @BeforeEach
+  fun setup() {
+    // TODO link this to the application-test.yml
+    mockServer.start(1234)
+  }
+
+  @AfterEach
+  fun cleanup() {
+    mockServer.shutdown()
+  }
+
   @Test
   fun `downloads and formats google sheet data`() {
     TODO()
