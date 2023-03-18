4de761cdf45e33b6d7c85246302b94d51877407d
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:23:15 -0400

    Refactoring download

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
@@ -1,7 +1,6 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
 import io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller.CraigMillerDownloaderService
-import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.springframework.scheduling.annotation.Scheduled
@@ -11,14 +10,8 @@ import org.springframework.stereotype.Service
 class DownloaderSchedulingService(
     private val craigMillerDownloaderService: CraigMillerDownloaderService
 ) {
-  @Scheduled(cron = "\${downloaders.interval-cron}")
-  fun executeDownloads() {
-    GlobalScope.launch { craigMillerDownloaderService.download() }
-  }
-
-  // TODO delete this
-  @PostConstruct
-  fun forTestingOnly() {
+  @Scheduled(fixedRateString = "#{\${downloaders.interval-rate-hours} * 60 * 60 * 1000}")
+  fun downloadAtInterval() {
     GlobalScope.launch { craigMillerDownloaderService.download() }
   }
 }
