16bbdbd5df2586d4004ea05c302d6782122e2b99
Craig Miller <craigmiller160@gmail.com>
2023-03-18 13:57:41 -0400

    More test setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/MarketTrackerPortfolioIntegrationTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/MarketTrackerPortfolioIntegrationTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @Retention(AnnotationRetention.RUNTIME)
@@ -13,4 +14,5 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @ExtendWith(value = [TestcontainersExtension::class, SpringExtension::class])
 @AutoConfigureWebTestClient
 @ActiveProfiles("test")
+@TestPropertySource(value = ["classpath:application.yml", "classpath:application-test.yml"])
 annotation class MarketTrackerPortfolioIntegrationTest
