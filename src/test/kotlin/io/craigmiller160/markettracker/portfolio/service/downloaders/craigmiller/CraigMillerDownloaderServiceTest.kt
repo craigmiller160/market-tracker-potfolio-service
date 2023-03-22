26fab2d701e00110ac8336459dff601f2ce8dbd7
Craig Miller <craigmiller160@gmail.com>
2023-03-21 20:02:24 -0400

    Working on tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
@@ -1,7 +1,13 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
+import io.craigmiller160.markettracker.portfolio.config.CraigMillerDownloaderConfig
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
+import java.math.BigDecimal
+import java.time.LocalDate
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
@@ -12,12 +18,52 @@ import org.springframework.beans.factory.annotation.Value
 @MarketTrackerPortfolioIntegrationTest
 class CraigMillerDownloaderServiceTest(
     @Value("\${downloaders.craigmiller.test-port}") private val testPort: Int,
-    private val service: CraigMillerDownloaderService
+    private val service: CraigMillerDownloaderService,
+    private val objectMapper: ObjectMapper,
+    private val downloaderConfig: CraigMillerDownloaderConfig,
 ) {
   companion object {
     private val transactions1: String = DataLoader.load("data/craigmiller/Transactions1.json")
-    // TODO delete second one if unnecessary
-    private val transactions2: String = DataLoader.load("data/craigmiller/Transactions2.json")
+    private val expectedData: List<SharesOwned> =
+        listOf(
+            createSharesOwned(
+                LocalDate.of(2020, 6, 4), LocalDate.of(2020, 6, 18), "COTY", BigDecimal("1")),
+            createSharesOwned(
+                LocalDate.of(2020, 6, 19),
+                CraigMillerDownloaderService.MAX_DATE,
+                "COTY",
+                BigDecimal("0")),
+            createSharesOwned(
+                LocalDate.of(2020, 6, 15),
+                CraigMillerDownloaderService.MAX_DATE,
+                "UAL",
+                BigDecimal("0")),
+            createSharesOwned(
+                LocalDate.of(2020, 6, 10), LocalDate.of(2020, 6, 14), "VYM", BigDecimal("1")),
+            createSharesOwned(
+                LocalDate.of(2020, 6, 15), LocalDate.of(2020, 6, 21), "VYM", BigDecimal("2")),
+            createSharesOwned(
+                LocalDate.of(2020, 6, 22), LocalDate.of(2020, 10, 17), "VYM", BigDecimal("4")),
+            createSharesOwned(
+                LocalDate.of(2020, 10, 18),
+                CraigMillerDownloaderService.MAX_DATE,
+                "VYM",
+                BigDecimal("6")))
+
+    private fun createSharesOwned(
+        start: LocalDate,
+        end: LocalDate,
+        symbol: String,
+        totalShares: BigDecimal
+    ): SharesOwned =
+        SharesOwned(
+            id = TypedId(),
+            portfolioId = TypedId(),
+            userId = TypedId(),
+            dateRangeStart = start,
+            dateRangeEnd = end,
+            totalShares = totalShares,
+            symbol = symbol)
   }
 
   private val mockServer = MockWebServer()
@@ -41,6 +87,8 @@ class CraigMillerDownloaderServiceTest(
             setBody(transactions1)
           })
     }
+
+    val values = objectMapper.readValue(transactions1, GoogleSpreadsheetValues::class.java)
     TODO()
   }
 }
