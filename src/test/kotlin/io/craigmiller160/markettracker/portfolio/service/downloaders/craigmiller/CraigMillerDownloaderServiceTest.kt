701a530335f7318441caf713034cf0959265d0c6
Craig Miller <craigmiller160@gmail.com>
2023-03-22 16:44:24 -0400

    Implementing more test logic

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderServiceTest.kt
@@ -3,9 +3,11 @@ package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmille
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.michaelbull.result.getOrThrow
 import io.craigmiller160.markettracker.portfolio.config.CraigMillerDownloaderConfig
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
 import io.kotest.matchers.collections.shouldBeIn
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.collections.shouldHaveSize
 import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
@@ -60,8 +62,19 @@ constructor(
       portfolio.name.shouldBeIn("Brokerage", "Roth IRA", "Rollover IRA")
       val expectedSharesOwned =
           TEST_DATA.map { it.copy(portfolioId = portfolio.id, userId = downloaderConfig.userId) }
-      portfolio.ownershipHistory.shouldHaveSize(expectedSharesOwned.size)
-      // TODO validate all records
+              .sortedWith(::sort)
+      portfolio.ownershipHistory
+          .sortedWith(::sort)
+          .shouldHaveSize(expectedSharesOwned.size)
+          .shouldContainAll(expectedSharesOwned)
     }
   }
+}
+
+private fun sort(one: SharesOwned, two: SharesOwned): Int {
+  val symbolCompare = one.symbol.compareTo(one.symbol)
+  if (symbolCompare == 0) {
+    return one.dateRangeStart.compareTo(two.dateRangeStart)
+  }
+  return symbolCompare
 }
