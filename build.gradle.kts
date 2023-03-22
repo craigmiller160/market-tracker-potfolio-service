c36eb90b3bbfaca8914eda9c9941abaf0b233702
Craig Miller <craigmiller160@gmail.com>
2023-03-22 15:44:04 -0400

    Added kotlin collection library

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
 
     testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.5")
 
 }
 
