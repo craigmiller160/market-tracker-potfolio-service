c37a751fcc20b05f9fd4ee895e832ac2155066e2
Craig Miller <craigmiller160@gmail.com>
2023-03-25 18:30:04 -0400

    More work on tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadServiceTest.kt
@@ -1,7 +1,15 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
+import com.github.michaelbull.result.getOrThrow
+import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
+import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
+import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
+import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+import java.math.BigDecimal
+import java.time.LocalDate
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,20 +23,39 @@ constructor(
     private val databaseClient: DatabaseClient,
     private val persistDownloadService: PersistDownloadService
 ) {
+  companion object {
+    private val PORTFOLIO_ID = TypedId<PortfolioId>()
+    private val USER_ID = TypedId<UserId>()
+    private val DATA: List<PortfolioWithHistory> =
+        listOf(
+            PortfolioWithHistory(
+                id = PORTFOLIO_ID,
+                userId = USER_ID,
+                name = "Portfolio",
+                ownershipHistory =
+                    listOf(
+                        SharesOwned(
+                            id = TypedId(),
+                            portfolioId = PORTFOLIO_ID,
+                            userId = USER_ID,
+                            dateRangeStart = LocalDate.of(2022, 1, 1),
+                            dateRangeEnd = LocalDate.of(2022, 2, 1),
+                            symbol = "VTI",
+                            totalShares = BigDecimal("10")))))
+  }
   @Test
   fun `the portfolios are all persisted`() {
     runBlocking {
-      getPortfolioCount().shouldBeEqualComparingTo(0)
+      getPortfolios().shouldBeEqualComparingTo(0)
       getSharesOwnedCount().shouldBeEqualComparingTo(0)
-      TODO()
+
+      persistDownloadService.persistPortfolios(DATA).getOrThrow()
     }
   }
 
-  private suspend fun getPortfolioCount(): Long =
-      databaseClient.sql("SELECT COUNT(*) AS count FROM portfolios").fetch().awaitOne()["count"]
-          as Long
+  private suspend fun getPortfolios(): Long =
+      databaseClient.sql("SELECT * FROM portfolios").fetch().awaitOne()["count"] as Long
 
   private suspend fun getSharesOwnedCount(): Long =
-      databaseClient.sql("SELECT COUNT(*) AS count FROM shares_owned").fetch().awaitOne()["count"]
-          as Long
+      databaseClient.sql("SELECT * FROM shares_owned").fetch().awaitOne()["count"] as Long
 }
