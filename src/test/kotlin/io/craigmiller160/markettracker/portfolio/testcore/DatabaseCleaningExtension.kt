fcf6d004dc1bc25df588fa79a3b6d4a512caa560
Craig Miller <craigmiller160@gmail.com>
2023-03-25 17:53:33 -0400

    Wrote db cleaning logic

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/DatabaseCleaningExtension.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcore/DatabaseCleaningExtension.kt
@@ -1,28 +1,33 @@
 package io.craigmiller160.markettracker.portfolio.testcore
 
-import io.craigmiller160.markettracker.portfolio.domain.sql.SqlLoader
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.extension.AfterEachCallback
 import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitRowsUpdated
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 class DatabaseCleaningExtension : BeforeEachCallback, AfterEachCallback {
+  companion object {
+    private const val DELETE_PORTFOLIOS_SQL = "DELETE FROM portfolios"
+    private const val DELETE_SHARES_OWNED_SQL = "DELETE FROM shares_owned"
+  }
   override fun beforeEach(context: ExtensionContext) {
-    cleanDatabase(context)
+    runBlocking { cleanDatabase(context) }
   }
 
   override fun afterEach(context: ExtensionContext) {
-    cleanDatabase(context)
+    runBlocking { cleanDatabase(context) }
   }
 
   private fun getDatabaseClient(context: ExtensionContext) =
       SpringExtension.getApplicationContext(context).getBean(DatabaseClient::class.java)
-  private fun getSqlLoader(context: ExtensionContext) =
-      SpringExtension.getApplicationContext(context).getBean(SqlLoader::class.java)
 
-  private fun cleanDatabase(context: ExtensionContext) {
+  private suspend fun cleanDatabase(context: ExtensionContext) {
     val databaseClient = getDatabaseClient(context)
-    val sqlLoader = getSqlLoader(context)
+    databaseClient.sql(DELETE_SHARES_OWNED_SQL).fetch().awaitRowsUpdated()
+
+    databaseClient.sql(DELETE_PORTFOLIOS_SQL).fetch().awaitRowsUpdated()
   }
 }
