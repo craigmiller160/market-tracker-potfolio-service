029140321307eedc29ac09fb43c33e5206b090cc
Craig Miller <craigmiller160@gmail.com>
2023-03-25 17:08:57 -0400

    Beginning work on service

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
@@ -1,5 +1,7 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
+import com.github.michaelbull.result.combine
+import com.github.michaelbull.result.map
 import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
@@ -9,7 +11,13 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 class PersistDownloadService(private val portfolioRepository: PortfolioRepository) {
   @Transactional
-  fun persistPortfolios(portfolios: List<PortfolioWithHistory>): KtResult<Unit> {
-    TODO()
+  suspend fun persistPortfolios(
+      portfolios: List<PortfolioWithHistory>
+  ): KtResult<List<PortfolioWithHistory>> = portfolios.map { createPortfolio(it) }.combine()
+
+  private suspend fun createPortfolio(
+      portfolio: PortfolioWithHistory
+  ): KtResult<PortfolioWithHistory> {
+    return portfolioRepository.createPortfolio(portfolio).map { portfolio }
   }
 }
