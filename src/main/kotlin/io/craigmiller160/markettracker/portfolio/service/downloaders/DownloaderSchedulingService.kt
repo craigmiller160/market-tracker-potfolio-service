6ba9d791103f59ffc9370170293a2b65fd386a87
Craig Miller <craigmiller160@gmail.com>
2023-03-25 17:12:46 -0400

    Updated service

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
@@ -1,8 +1,11 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
+import com.github.michaelbull.result.flatMap
+import com.github.michaelbull.result.map
+import com.github.michaelbull.result.onFailure
+import io.craigmiller160.markettracker.portfolio.functions.KtResult
 import io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller.CraigMillerDownloaderService
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
@@ -10,10 +13,16 @@ import org.springframework.stereotype.Service
 @Service
 @ConditionalOnProperty(name = ["downloaders.enable"], havingValue = "")
 class DownloaderSchedulingService(
-    private val craigMillerDownloaderService: CraigMillerDownloaderService
+    private val craigMillerDownloaderService: CraigMillerDownloaderService,
+    private val persistDownloadService: PersistDownloadService
 ) {
+  private val log = LoggerFactory.getLogger(javaClass)
+
   @Scheduled(fixedRateString = "#{\${downloaders.interval-rate-hours} * 60 * 60 * 1000}")
-  fun downloadAtInterval() {
-    GlobalScope.launch { craigMillerDownloaderService.download() }
-  }
+  suspend fun downloadAtInterval(): KtResult<Unit> =
+      craigMillerDownloaderService
+          .download()
+          .flatMap { persistDownloadService.persistPortfolios(it) }
+          .onFailure { ex -> log.error("Error downloading portfolio data", ex) }
+          .map { Unit }
 }
