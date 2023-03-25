3c62af8f1b407a53ffabf601929bfe09613c5cef
Craig Miller <craigmiller160@gmail.com>
2023-03-25 11:10:29 -0400

    Some work on repo

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientPortfolioRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientPortfolioRepository.kt
@@ -2,14 +2,21 @@ package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
+import io.craigmiller160.markettracker.portfolio.domain.sql.SqlLoader
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 
 @Repository
-class DatabaseClientPortfolioRepository(private val databaseClient: DatabaseClient) :
-    PortfolioRepository {
+class DatabaseClientPortfolioRepository(
+    private val databaseClient: DatabaseClient,
+    private val sqlLoader: SqlLoader
+) : PortfolioRepository {
+  companion object {
+    private const val INSERT_PORTFOLIO_SQL = "portfolio/insertPortfolio.sql"
+  }
   override suspend fun createPortfolio(portfolio: Portfolio): KtResult<Portfolio> {
-    TODO("Not yet implemented")
+    sqlLoader.loadSql(INSERT_PORTFOLIO_SQL)
+    TODO()
   }
 }
