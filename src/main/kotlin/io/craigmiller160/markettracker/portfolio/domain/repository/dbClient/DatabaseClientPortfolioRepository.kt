f53fed5059f935bec82be7193ec31e229b359844
Craig Miller <craigmiller160@gmail.com>
2023-03-25 11:18:29 -0400

    Implemented repo method

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientPortfolioRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientPortfolioRepository.kt
@@ -1,10 +1,14 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
+import com.github.michaelbull.result.flatMap
+import com.github.michaelbull.result.map
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
 import io.craigmiller160.markettracker.portfolio.domain.sql.SqlLoader
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
+import io.craigmiller160.markettracker.portfolio.functions.ktRunCatching
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitRowsUpdated
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -15,8 +19,21 @@ class DatabaseClientPortfolioRepository(
   companion object {
     private const val INSERT_PORTFOLIO_SQL = "portfolio/insertPortfolio.sql"
   }
-  override suspend fun createPortfolio(portfolio: Portfolio): KtResult<Portfolio> {
-    sqlLoader.loadSql(INSERT_PORTFOLIO_SQL)
-    TODO()
-  }
+
+  // TODO need to test spring transaction management
+  override suspend fun createPortfolio(portfolio: Portfolio): KtResult<Portfolio> =
+      sqlLoader
+          .loadSql(INSERT_PORTFOLIO_SQL)
+          .flatMap { sql ->
+            ktRunCatching {
+              databaseClient
+                  .sql(sql)
+                  .bind("id", portfolio.id)
+                  .bind("userId", portfolio.userId)
+                  .bind("name", portfolio.name)
+                  .fetch()
+                  .awaitRowsUpdated()
+            }
+          }
+          .map { portfolio }
 }
