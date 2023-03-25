5f5ee04a6d15fff9f2ed9c7dc974748c83f6b692
Craig Miller <craigmiller160@gmail.com>
2023-03-25 15:46:58 -0400

    Validated transactional behavior with r2dbc

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/controllers/PracticeController2.kt
@@ -6,8 +6,10 @@ import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
 import io.craigmiller160.markettracker.portfolio.web.types.PracticeResponse
 import java.util.UUID
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController
 class PracticeController2(private val repo: PortfolioRepository) {
   @GetMapping suspend fun hello(): PracticeResponse = PracticeResponse("Hello World 2")
 
+  @Transactional
   @GetMapping("/experiment")
-  suspend fun transactionExp() {
+  suspend fun transactionExp(@RequestParam("throwEx") throwEx: Boolean) {
     val unique = UUID.randomUUID()
     val portfolio = BasePortfolio(id = TypedId(), userId = TypedId(), name = "Hello_$unique")
     repo.createPortfolio(portfolio).onFailure { it.printStackTrace() }
+    if (throwEx) {
+      throw RuntimeException()
+    }
   }
 }
