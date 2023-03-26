2e892e380b299b6a381417940c4329714a7e76d1
Craig Miller <craigmiller160@gmail.com>
2023-03-26 17:31:04 -0400

    Added todo

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/common/typedid/converter/Converters.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/common/typedid/converter/Converters.kt
@@ -5,6 +5,8 @@ import java.util.UUID
 import org.springframework.core.convert.converter.Converter
 import org.springframework.stereotype.Component
 
+// TODO https://stackoverflow.com/questions/64351344/r2dbc-custom-converter
+
 @Component
 class StringToTypedIdConverter : Converter<String, TypedId<*>> {
   override fun convert(source: String): TypedId<*> = TypedId<Any>(source)
