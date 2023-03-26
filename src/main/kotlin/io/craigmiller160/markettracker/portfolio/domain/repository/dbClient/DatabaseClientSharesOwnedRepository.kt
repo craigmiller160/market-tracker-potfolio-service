2bdcee84227f7c89919bad84a9dc78b27bc0d9f5
Craig Miller <craigmiller160@gmail.com>
2023-03-26 12:05:55 -0400

    Implemented repo logic

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
@@ -1,11 +1,15 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
+import com.github.michaelbull.result.flatMap
+import com.github.michaelbull.result.map
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.domain.sql.SqlLoader
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
+import io.craigmiller160.markettracker.portfolio.functions.ktRunCatching
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
+import reactor.kotlin.core.publisher.toFlux
 
 @Repository
 class DatabaseClientSharesOwnedRepository(
@@ -17,7 +21,27 @@ class DatabaseClientSharesOwnedRepository(
   }
   override suspend fun createAllSharesOwned(
       sharesOwned: List<SharesOwned>
-  ): KtResult<List<SharesOwned>> {
-    TODO("Not yet implemented")
-  }
+  ): KtResult<List<SharesOwned>> =
+      sqlLoader
+          .loadSql(INSERT_SHARES_OWNED_SQL)
+          .flatMap { sql ->
+            ktRunCatching {
+              databaseClient.inConnectionMany { conn ->
+                val stmt = conn.createStatement(sql)
+                sharesOwned.forEach { record ->
+                  stmt
+                      .bind("id", record.id.value)
+                      .bind("userId", record.userId.value)
+                      .bind("portfolioId", record.portfolioId.value)
+                      .bind("dateRangeStart", record.dateRangeStart)
+                      .bind("dateRangeEnd", record.dateRangeEnd)
+                      .bind("symbol", record.symbol)
+                      .bind("totalShares", record.totalShares)
+                      .add()
+                }
+                stmt.execute().toFlux()
+              }
+            }
+          }
+          .map { sharesOwned }
 }
