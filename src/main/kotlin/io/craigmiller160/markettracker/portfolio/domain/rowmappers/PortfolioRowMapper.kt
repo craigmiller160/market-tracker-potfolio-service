4e810ed58d49ca86a42d2456a85f596a771a7270
Craig Miller <craigmiller160@gmail.com>
2023-03-26 18:36:09 -0400

    Implemented more row mapping logic

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
@@ -3,11 +3,14 @@ package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
+import io.craigmiller160.markettracker.portfolio.functions.ktRunCatching
 import java.util.UUID
 
 val portfolioRowMapper: RowMapper<Portfolio> = { row, metadata ->
-  BasePortfolio(
-      id = TypedId(row.get("id", UUID::class.java)!!),
-      userId = TypedId(row.get("user_id", UUID::class.java)!!),
-      name = row.get("name", String::class.java)!!)
+  ktRunCatching {
+    BasePortfolio(
+        id = TypedId(row.get("id", UUID::class.java)!!),
+        userId = TypedId(row.get("user_id", UUID::class.java)!!),
+        name = row.get("name", String::class.java)!!)
+  }
 }
--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
@@ -1,8 +1,24 @@
 package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 
+import com.github.michaelbull.result.flatMap
+import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
+import io.craigmiller160.markettracker.portfolio.functions.ktRunCatching
+import java.math.BigDecimal
+import java.util.UUID
 
 val sharesOwnedRowMapper: RowMapper<SharesOwned> = { row, metadata ->
-  println("foo")
-  TODO()
+  dateRangeTypeParser(row.get("date_range", String::class.java)!!).flatMap {
+      (dateRangeStart, dateRangeEnd) ->
+    ktRunCatching {
+      SharesOwned(
+          id = TypedId(row.get("id", UUID::class.java)!!),
+          userId = TypedId(row.get("user_id", UUID::class.java)!!),
+          portfolioId = TypedId(row.get("portfolio_id", UUID::class.java)!!),
+          dateRangeStart = dateRangeStart,
+          dateRangeEnd = dateRangeEnd,
+          symbol = row.get("symbol", String::class.java)!!,
+          totalShares = row.get("total_shares", BigDecimal::class.java)!!)
+    }
+  }
 }
