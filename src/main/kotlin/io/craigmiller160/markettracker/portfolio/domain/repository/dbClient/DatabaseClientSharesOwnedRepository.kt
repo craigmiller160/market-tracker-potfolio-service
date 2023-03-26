930ca3b7bfbf712916d1da5bad15c356d7b0d4a2
Craig Miller <craigmiller160@gmail.com>
2023-03-26 17:55:36 -0400

    Finished with big refactor of db logic

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
@@ -1,12 +1,12 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
-import com.github.michaelbull.result.flatMap
 import com.github.michaelbull.result.map
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.domain.models.dateRange
 import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.domain.sql.SqlLoader
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
+import io.craigmiller160.markettracker.portfolio.functions.coFlatMap
 import io.craigmiller160.markettracker.portfolio.functions.ktRunCatching
 import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.r2dbc.core.DatabaseClient
@@ -24,33 +24,40 @@ class DatabaseClientSharesOwnedRepository(
   override suspend fun createAllSharesOwned(
       sharesOwned: List<SharesOwned>
   ): KtResult<List<SharesOwned>> =
-      sqlLoader
-          .loadSql(INSERT_SHARES_OWNED_SQL)
-          .flatMap { sql ->
-            ktRunCatching {
-              databaseClient
-                  .inConnection { conn ->
-                    val stmt = conn.createStatement(sql)
-                    // TODO definitely refactor to be flexible
+      sqlLoader.loadSql(INSERT_SHARES_OWNED_SQL).coFlatMap(createAsBatch(sharesOwned)).map {
+        sharesOwned
+      }
+
+  private suspend fun createAsBatch(
+      sharesOwned: List<SharesOwned>
+  ): suspend (String) -> KtResult<Long> = { sql ->
+    ktRunCatching {
+      databaseClient
+          .inConnection { conn ->
+            val stmt = conn.createStatement(sql)
+            sharesOwned
+                .map { record ->
+                  {
                     stmt
-                        .bind(0, sharesOwned[0].id.value)
-                        .bind(1, sharesOwned[0].userId.value)
-                        .bind(2, sharesOwned[0].portfolioId.value)
-                        .bind(3, sharesOwned[0].dateRange)
-                        .bind(4, sharesOwned[0].symbol)
-                        .bind(5, sharesOwned[0].totalShares)
-                        .add()
-                        .bind(0, sharesOwned[1].id.value)
-                        .bind(1, sharesOwned[1].userId.value)
-                        .bind(2, sharesOwned[1].portfolioId.value)
-                        .bind(3, sharesOwned[1].dateRange)
-                        .bind(4, sharesOwned[1].symbol)
-                        .bind(5, sharesOwned[1].totalShares)
-                    stmt.execute().toMono()
+                        .bind(0, record.id.value)
+                        .bind(1, record.userId.value)
+                        .bind(2, record.portfolioId.value)
+                        .bind(3, record.dateRange)
+                        .bind(4, record.symbol)
+                        .bind(5, record.totalShares)
                   }
-                  .awaitSingle()
-            }
+                }
+                .reduce { first, second ->
+                  {
+                    first().add()
+                    second()
+                  }
+                }
+            stmt.execute().toMono()
           }
-          .map { result -> result.rowsUpdated.awaitSingle() }
-          .map { sharesOwned }
+          .awaitSingle()
+          .rowsUpdated
+          .awaitSingle()
+    }
+  }
 }
