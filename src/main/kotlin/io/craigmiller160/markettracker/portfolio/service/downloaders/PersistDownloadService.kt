44b4b1d267fa2e95440db49d00968ca897b01efe
Craig Miller <craigmiller160@gmail.com>
2023-03-26 11:35:19 -0400

    Integrated new repo into service

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
@@ -1,15 +1,20 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders
 
 import com.github.michaelbull.result.combine
+import com.github.michaelbull.result.flatMap
 import com.github.michaelbull.result.map
 import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
+import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.functions.KtResult
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PersistDownloadService(private val portfolioRepository: PortfolioRepository) {
+class PersistDownloadService(
+    private val portfolioRepository: PortfolioRepository,
+    private val sharesOwnedRepository: SharesOwnedRepository
+) {
   @Transactional
   suspend fun persistPortfolios(
       portfolios: List<PortfolioWithHistory>
@@ -17,7 +22,9 @@ class PersistDownloadService(private val portfolioRepository: PortfolioRepositor
 
   private suspend fun createPortfolio(
       portfolio: PortfolioWithHistory
-  ): KtResult<PortfolioWithHistory> {
-    return portfolioRepository.createPortfolio(portfolio).map { portfolio }
-  }
+  ): KtResult<PortfolioWithHistory> =
+      portfolioRepository
+          .createPortfolio(portfolio)
+          .flatMap { sharesOwnedRepository.createAllSharesOwned(portfolio.ownershipHistory) }
+          .map { portfolio }
 }
