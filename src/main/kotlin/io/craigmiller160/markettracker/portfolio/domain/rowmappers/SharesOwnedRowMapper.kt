874ef6dfabad1ca07ac73fdce061dc0cb48eab47
Craig Miller <craigmiller160@gmail.com>
2023-03-26 11:50:03 -0400

    implemented test

---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
@@ -0,0 +1,5 @@
+package io.craigmiller160.markettracker.portfolio.domain.rowmappers
+
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
+
+val sharesOwnedRowMapper: RowMapper<SharesOwned> = TODO()
--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadServiceTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadServiceTest.kt
@@ -8,6 +8,7 @@ import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
 import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.domain.rowmappers.portfolioRowMapper
+import io.craigmiller160.markettracker.portfolio.domain.rowmappers.sharesOwnedRowMapper
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.equality.shouldBeEqualToComparingFields
@@ -46,12 +47,21 @@ constructor(
                             dateRangeStart = LocalDate.of(2022, 1, 1),
                             dateRangeEnd = LocalDate.of(2022, 2, 1),
                             symbol = "VTI",
-                            totalShares = BigDecimal("10")))))
+                            totalShares = BigDecimal("10")),
+                        SharesOwned(
+                            id = TypedId(),
+                            portfolioId = PORTFOLIO_ID,
+                            userId = USER_ID,
+                            dateRangeStart = LocalDate.of(2022, 2, 1),
+                            dateRangeEnd = LocalDate.of(2022, 3, 1),
+                            symbol = "VTI",
+                            totalShares = BigDecimal("20")))))
   }
   @Test
   fun `the portfolios are all persisted`() {
     runBlocking {
       getPortfolios().toList().shouldHaveSize(0)
+      getSharesOwned().toList().shouldHaveSize(0)
 
       persistDownloadService.persistPortfolios(DATA).getOrThrow()
 
@@ -62,13 +72,27 @@ constructor(
         portfolio.userId.shouldBeEqualToComparingFields(DATA[0].userId)
         portfolio.name.shouldBeEqualToComparingFields(DATA[0].name)
       }
+
+      val sharesOwned = getSharesOwned().toList()
+      sharesOwned.shouldHaveSize(2)
+      sharesOwned.forEachIndexed { index, actual ->
+        actual.id.shouldBeEqualToComparingFields(DATA[0].ownershipHistory[index].id)
+        actual.userId.shouldBeEqualToComparingFields(DATA[0].ownershipHistory[index].userId)
+        actual.portfolioId.shouldBeEqualToComparingFields(
+            DATA[0].ownershipHistory[index].portfolioId)
+        actual.dateRangeStart.shouldBeEqualToComparingFields(
+            DATA[0].ownershipHistory[index].dateRangeStart)
+        actual.dateRangeEnd.shouldBeEqualToComparingFields(
+            DATA[0].ownershipHistory[index].dateRangeEnd)
+        actual.symbol.shouldBeEqualToComparingFields(DATA[0].ownershipHistory[index].symbol)
+        actual.totalShares.shouldBeEqualToComparingFields(
+            DATA[0].ownershipHistory[index].totalShares)
+      }
     }
   }
 
   private suspend fun getPortfolios(): Flow<Portfolio> =
       databaseClient.sql("SELECT * FROM portfolios").map(portfolioRowMapper).all().asFlow()
-  private suspend fun getSharesOwned(): List<SharesOwned> {
-    databaseClient.sql("SELECT * FROM shares_owned").fetch().all()
-    TODO()
-  }
+  private suspend fun getSharesOwned(): Flow<SharesOwned> =
+      databaseClient.sql("SELECT * FROM shares_owned").map(sharesOwnedRowMapper).all().asFlow()
 }
