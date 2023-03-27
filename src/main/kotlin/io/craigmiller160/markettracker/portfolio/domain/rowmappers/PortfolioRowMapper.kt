ac973487defbb9c4e9b962485daa10f2f8b7b752
Craig Miller <craigmiller160@gmail.com>
2023-03-27 08:29:36 -0400

    Improved mapper error handling

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
@@ -4,15 +4,14 @@ import arrow.core.continuations.either
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
-import io.craigmiller160.markettracker.portfolio.extensions.leftIfNull
+import io.craigmiller160.markettracker.portfolio.extensions.getRequired
 import java.util.UUID
 
 val portfolioRowMapper: RowMapper<Portfolio> = { row, metadata ->
   either.eager {
-    // TODO handle type exceptions
-    val id = row.get("id", UUID::class.java).leftIfNull("Missing id column").bind()
-    val userId = row.get("user_id", UUID::class.java).leftIfNull("Missing user_id column").bind()
-    val name = row.get("name", String::class.java).leftIfNull("Missing name column").bind()
+    val id = row.getRequired("id", UUID::class.java).bind()
+    val userId = row.getRequired("user_id", UUID::class.java).bind()
+    val name = row.getRequired("name", String::class.java).bind()
 
     BasePortfolio(id = TypedId(id), userId = TypedId(userId), name = name)
   }
---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/extensions/RowExtensions.kt
@@ -0,0 +1,8 @@
+package io.craigmiller160.markettracker.portfolio.extensions
+
+import arrow.core.Either
+import arrow.core.flatMap
+import io.r2dbc.spi.Row
+
+fun <T> Row.getRequired(name: String, type: Class<T>): TryEither<T> =
+    Either.catch { get(name, type) }.flatMap { it.leftIfNull("Missing $name column") }
