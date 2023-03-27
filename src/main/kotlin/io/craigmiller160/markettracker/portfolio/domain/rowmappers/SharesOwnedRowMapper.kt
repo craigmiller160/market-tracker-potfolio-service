1f986ef24783524f43f3fe2fa20dd4317fa091b7
Craig Miller <craigmiller160@gmail.com>
2023-03-26 20:29:23 -0400

    Refactored row mapper

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
@@ -1,8 +1,6 @@
 package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 
 import arrow.core.continuations.either
-import arrow.core.continuations.option
-import arrow.core.toOption
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.extensions.leftIfNull
@@ -24,26 +22,14 @@ val sharesOwnedRowMapper: RowMapper<SharesOwned> = { row, metadata ->
             .bind()
 
     val (dateRangeStart, dateRangeEnd) = dateRangeTypeParser(dateRangeString).bind()
-  }
 
-  option.eager {
-    val dateRangeString = row.get("date_range", String::class.java).toOption().bind()
-    val id = row.get("id", UUID::class.java).toOption().bind()
-    val userId = row.get("user_id", UUID::class.java)
-
-  }
-
-  dateRangeTypeParser(row.get("date_range", String::class.java)!!).flatMap {
-      (dateRangeStart, dateRangeEnd) ->
-    ktRunCatching {
-      SharesOwned(
-          id = TypedId(row.get("id", UUID::class.java)!!),
-          userId = TypedId(row.get("user_id", UUID::class.java)!!),
-          portfolioId = TypedId(row.get("portfolio_id", UUID::class.java)!!),
-          dateRangeStart = dateRangeStart,
-          dateRangeEnd = dateRangeEnd,
-          symbol = row.get("symbol", String::class.java)!!,
-          totalShares = row.get("total_shares", BigDecimal::class.java)!!)
-    }
+    SharesOwned(
+        id = TypedId(id),
+        userId = TypedId(userId),
+        portfolioId = TypedId(portfolioId),
+        dateRangeStart = dateRangeStart,
+        dateRangeEnd = dateRangeEnd,
+        symbol = symbol,
+        totalShares = totalShares)
   }
 }
