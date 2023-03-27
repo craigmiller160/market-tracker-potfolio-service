700403270b64decce252a92dc9d287c7cb5e1a92
Craig Miller <craigmiller160@gmail.com>
2023-03-26 22:26:49 -0400

    Renamed class

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapperTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapperTest.kt
@@ -2,7 +2,7 @@ package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 
 import org.junit.jupiter.api.Test
 
-class SharesOwnedRowMapper {
+class SharesOwnedRowMapperTest {
   @Test
   fun `maps SharesOwned from row`() {
     TODO()
