6a0c091d9f737b64e3cc86d43c4ff09acd1eddc6
Craig Miller <craigmiller160@gmail.com>
2023-03-26 20:32:50 -0400

    Finished refactoring row mapper

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapper.kt
@@ -1,6 +1,5 @@
 package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 
-import arrow.core.Either
 import arrow.core.continuations.either
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
@@ -10,15 +9,10 @@ import java.util.UUID
 
 val portfolioRowMapper: RowMapper<Portfolio> = { row, metadata ->
   either.eager {
-    row.get("id", UUID::class.java).leftIfNull("Missing id column").bind()
-    row.get("user_id", UUID::class.java).leftIfNull("Missing user_id column").bind()
-    row.get("name", String::class.java).leftIfNull("Missing name column").bind()
-  }
+    val id = row.get("id", UUID::class.java).leftIfNull("Missing id column").bind()
+    val userId = row.get("user_id", UUID::class.java).leftIfNull("Missing user_id column").bind()
+    val name = row.get("name", String::class.java).leftIfNull("Missing name column").bind()
 
-  Either.catch {
-    BasePortfolio(
-        id = TypedId(row.get("id", UUID::class.java)!!),
-        userId = TypedId(row.get("user_id", UUID::class.java)!!),
-        name = row.get("name", String::class.java)!!)
+    BasePortfolio(id = TypedId(id), userId = TypedId(userId), name = name)
   }
 }
