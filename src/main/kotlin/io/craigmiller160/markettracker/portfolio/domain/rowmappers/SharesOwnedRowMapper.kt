017557dbeea13812db495a3a8ce65551f25aa45e
Craig Miller <craigmiller160@gmail.com>
2023-03-28 17:17:43 -0400

    Refactored row mapper

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedRowMapper.kt
@@ -3,24 +3,18 @@ package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 import arrow.core.continuations.either
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
-import io.craigmiller160.markettracker.portfolio.extensions.leftIfNull
+import io.craigmiller160.markettracker.portfolio.extensions.getRequired
 import java.math.BigDecimal
 import java.util.UUID
 
 val sharesOwnedRowMapper: RowMapper<SharesOwned> = { row, metadata ->
   either.eager {
-    // TODO handle type exceptions
-    val dateRangeString =
-        row.get("date_range", String::class.java).leftIfNull("Missing date_range column").bind()
-    val id = row.get("id", UUID::class.java).leftIfNull("Missing id column").bind()
-    val userId = row.get("user_id", UUID::class.java).leftIfNull("Missing user_id column").bind()
-    val portfolioId =
-        row.get("portfolio_id", UUID::class.java).leftIfNull("Missing portfolio_id column").bind()
-    val symbol = row.get("symbol", String::class.java).leftIfNull("Missing symbol column").bind()
-    val totalShares =
-        row.get("total_shares", BigDecimal::class.java)
-            .leftIfNull("Missing total_shares column")
-            .bind()
+    val dateRangeString = row.getRequired("date_range", String::class.java).bind()
+    val id = row.getRequired("id", UUID::class.java).bind()
+    val userId = row.getRequired("user_id", UUID::class.java).bind()
+    val portfolioId = row.getRequired("portfolio_id", UUID::class.java).bind()
+    val symbol = row.getRequired("symbol", String::class.java).bind()
+    val totalShares = row.getRequired("total_shares", BigDecimal::class.java).bind()
 
     val (dateRangeStart, dateRangeEnd) = dateRangeTypeParser(dateRangeString).bind()
 
