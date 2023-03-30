e9aac5294e4c4df968706546d873d22b1951fa06
Craig Miller <craigmiller160@gmail.com>
2023-03-30 08:49:07 -0400

    Stubbed out new tests

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/response/EitherToResponseTest.kt
@@ -0,0 +1,25 @@
+package io.craigmiller160.markettracker.portfolio.web.response
+
+import org.junit.jupiter.api.Test
+
+class EitherToResponseTest {
+  @Test
+  fun `converts a Right into a 200 response`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts a Left into a 500 response`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts a Right into a 202 response with custom headers`() {
+    TODO()
+  }
+
+  @Test
+  fun `converts a Left into a 400 response with custom headers`() {
+    TODO()
+  }
+}
