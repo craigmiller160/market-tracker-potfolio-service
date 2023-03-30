eeaae1d358ce7e68b4cbea45b0632b7bf34a9f63
Craig Miller <craigmiller160@gmail.com>
2023-03-30 13:26:02 -0400

    Making some more progress

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandler.kt
@@ -1,16 +1,32 @@
 package io.craigmiller160.markettracker.portfolio.web.handlers
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.markettracker.portfolio.web.types.ErrorResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.server.WebExceptionHandler
 
 @Component
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(private val objectMapper: ObjectMapper) {
   @Bean
   @Order(-2)
   fun exceptionHandler(): WebExceptionHandler = WebExceptionHandler { exchange, ex ->
-    exchange.response.apply { statusCode = HttpStatus.BAD_REQUEST }.setComplete()
+    val status = HttpStatus.INTERNAL_SERVER_ERROR
+    val response =
+        ErrorResponse(
+            method = exchange.request.method.name(),
+            uri = exchange.request.uri.toString(),
+            message = ex.message ?: "",
+            status = status.value())
+    exchange.response
+        .apply {
+          statusCode = status
+          headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        }
+        .writeWith()
   }
 }
