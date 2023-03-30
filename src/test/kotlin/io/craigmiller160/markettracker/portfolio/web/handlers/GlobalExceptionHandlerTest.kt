32f61dbfe68a86deb9453917d56c2861b455cce3
Craig Miller <craigmiller160@gmail.com>
2023-03-30 13:27:16 -0400

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandlerTest.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.markettracker.portfolio.web.handlers
+
+import org.junit.jupiter.api.Test
+
+class GlobalExceptionHandlerTest {
+  @Test
+  fun `internal server error`() {
+    TODO()
+  }
+}
