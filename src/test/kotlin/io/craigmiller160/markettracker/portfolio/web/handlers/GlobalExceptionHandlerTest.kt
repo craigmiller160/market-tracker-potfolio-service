f09a833477d7a574f59efb4361503dbe1aabe232
Craig Miller <craigmiller160@gmail.com>
2023-03-31 08:53:26 -0400

    Implemented test

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandlerTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandlerTest.kt
@@ -1,10 +1,35 @@
 package io.craigmiller160.markettracker.portfolio.web.handlers
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import io.craigmiller160.markettracker.portfolio.testutils.DefaultUsers
+import io.craigmiller160.markettracker.portfolio.web.types.ErrorResponse
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.web.reactive.server.WebTestClient
 
-class GlobalExceptionHandlerTest {
+@MarketTrackerPortfolioIntegrationTest
+class GlobalExceptionHandlerTest
+@Autowired
+constructor(
+    private val webTestClient: WebTestClient,
+    private val defaultUsers: DefaultUsers,
+    private val objectMapper: ObjectMapper
+) {
   @Test
   fun `internal server error`() {
-    TODO()
+    val response =
+        ErrorResponse(
+            method = "GET", uri = "/throw-exception/runtime-exception", message = "", status = 500)
+
+    webTestClient
+        .get()
+        .uri("/throw-exception/runtime-exception")
+        .header("Authorization", "Bearer ${defaultUsers.primaryUser.token}")
+        .exchange()
+        .expectStatus()
+        .isEqualTo(500)
+        .expectBody()
+        .json(objectMapper.writeValueAsString(response))
   }
 }
