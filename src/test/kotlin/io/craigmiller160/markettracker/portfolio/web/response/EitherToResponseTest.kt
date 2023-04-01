683dfaeb1eb173d0db3ba8c8d156238ffbcb455f
Craig Miller <craigmiller160@gmail.com>
2023-04-01 16:39:21 -0400

    Put together more pieces of the tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/response/EitherToResponseTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/response/EitherToResponseTest.kt
@@ -5,15 +5,23 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.springframework.http.codec.HttpMessageWriter
+import org.springframework.mock.http.server.reactive.MockServerHttpRequest
+import org.springframework.mock.web.server.MockServerWebExchange
+import org.springframework.web.reactive.function.server.HandlerStrategies
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.result.view.ViewResolver
 
 class EitherToResponseTest {
   @Test
   fun `converts a Right into a 200 response`() {
     val response = runBlocking { Either.Right("Hello World").toResponse() }
     response.statusCode().value().shouldBe(200)
-    TODO("Figure out how to get the body")
+
+    runBlocking { response.getBody() }.shouldBe("Hello World")
   }
 
   @Test
@@ -30,6 +38,24 @@ class EitherToResponseTest {
     }
     response.statusCode().value().shouldBe(202)
     response.headers()["ABC"].shouldNotBeNull().shouldHaveSize(1).first().shouldBe("DEF")
-    TODO("Figure out how to get the body")
+
+    runBlocking { response.getBody() }.shouldBe("Hello World")
+  }
+
+  private suspend inline fun ServerResponse.getBody(): String {
+    val mockExchange =
+        MockServerHttpRequest.get("http://thisdoenstmatter.com").build().let {
+          MockServerWebExchange.from(it)
+        }
+    val mockContext =
+        object : ServerResponse.Context {
+          override fun messageWriters(): MutableList<HttpMessageWriter<*>> =
+              HandlerStrategies.withDefaults().messageWriters()
+
+          override fun viewResolvers(): MutableList<ViewResolver> = mutableListOf()
+        }
+
+    writeTo(mockExchange, mockContext)
+    return mockExchange.response.bodyAsString.awaitSingle()
   }
 }
