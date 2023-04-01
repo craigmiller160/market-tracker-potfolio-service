058f53081ae360f388aa1f41d9e37fd41f31b41e
Craig Miller <craigmiller160@gmail.com>
2023-04-01 17:36:42 -0400

    Split up scheduling and orchestration

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderOrchestrationService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderOrchestrationService.kt
@@ -3,22 +3,15 @@ package io.craigmiller160.markettracker.portfolio.service.downloaders
 import arrow.core.flatMap
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller.CraigMillerDownloaderService
-import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 
 @Service
-@ConditionalOnProperty(name = ["downloaders.enable-schedule"], havingValue = "true")
 class DownloaderOrchestrationService(
     private val craigMillerDownloaderService: CraigMillerDownloaderService,
     private val persistDownloadService: PersistDownloadService
 ) {
   private val log = LoggerFactory.getLogger(javaClass)
-
-  @Scheduled(fixedRateString = "#{\${downloaders.interval-rate-hours} * 60 * 60 * 1000}")
-  fun downloadAtInterval(): TryEither<Unit> = runBlocking { download() }
 
   suspend fun download(): TryEither<Unit> =
       craigMillerDownloaderService
---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
@@ -0,0 +1,16 @@
+package io.craigmiller160.markettracker.portfolio.service.downloaders
+
+import io.craigmiller160.markettracker.portfolio.extensions.TryEither
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.scheduling.annotation.Scheduled
+
+@ConditionalOnProperty(name = ["downloaders.enable-schedule"], havingValue = "true")
+class DownloaderSchedulingService(
+    private val downloaderOrchestrationService: DownloaderOrchestrationService
+) {
+  @Scheduled(fixedRateString = "#{\${downloaders.interval-rate-hours} * 60 * 60 * 1000}")
+  fun downloadAtInterval(): TryEither<Unit> = runBlocking {
+    downloaderOrchestrationService.download()
+  }
+}
