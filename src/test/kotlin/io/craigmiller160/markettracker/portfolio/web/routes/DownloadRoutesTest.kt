b080d630daedfa09baba295287f8b42c6222fbc8
Craig Miller <craigmiller160@gmail.com>
2023-04-01 17:48:45 -0400

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/DownloadRoutesTest.kt
@@ -0,0 +1,12 @@
+package io.craigmiller160.markettracker.portfolio.web.routes
+
+import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import org.junit.jupiter.api.Test
+
+@MarketTrackerPortfolioIntegrationTest
+class DownloadRoutesTest {
+  @Test
+  fun `downloads the remote data`() {
+    TODO()
+  }
+}
