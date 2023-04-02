457e0564b3dde3479fc38bff412f797d546ec554
Craig Miller <craigmiller160@gmail.com>
2023-04-02 12:25:20 -0400

    Got rollbacks working properly

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/PersistDownloadService.kt
@@ -7,17 +7,25 @@ import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepo
 import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.reactive.TransactionalOperator
+import org.springframework.transaction.reactive.executeAndAwait
 
 @Service
 class PersistDownloadService(
     private val portfolioRepository: PortfolioRepository,
-    private val sharesOwnedRepository: SharesOwnedRepository
+    private val sharesOwnedRepository: SharesOwnedRepository,
+    private val transactionOperator: TransactionalOperator
 ) {
-  @Transactional
+  // TODO delete all @Transactional uses
   suspend fun persistPortfolios(
       portfolios: List<PortfolioWithHistory>
-  ): TryEither<List<PortfolioWithHistory>> = portfolios.map { createPortfolio(it) }.sequence()
+  ): TryEither<List<PortfolioWithHistory>> =
+      transactionOperator.executeAndAwait { tx ->
+        portfolios
+            .map { createPortfolio(it) }
+            .sequence()
+            .onLeft { tx.setRollbackOnly() } // TODO create extension function to automate this
+      }
 
   private suspend fun createPortfolio(
       portfolio: PortfolioWithHistory
