23f914016a51d68caf337f80db9c322e49f9cb97
Craig Miller <craigmiller160@gmail.com>
2023-04-03 17:20:39 -0400

    Working on test setup logic

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesTest.kt
@@ -3,11 +3,15 @@ package io.craigmiller160.markettracker.portfolio.web.routes
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
+import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.domain.models.toPortfolioNameResponse
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
+import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DefaultUsers
 import io.craigmiller160.markettracker.portfolio.web.types.PortfolioStockResponse
+import java.math.BigDecimal
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -20,11 +24,12 @@ class PortfolioRoutesTest
 constructor(
     private val webTestClient: WebTestClient,
     private val portfolioRepo: PortfolioRepository,
+    private val sharesOwnedRepo: SharesOwnedRepository,
     private val defaultUsers: DefaultUsers,
     private val objectMapper: ObjectMapper
 ) {
 
-  private val portfolios =
+  private val portfolios: List<Portfolio> =
       (0 until 5).map { index ->
         BasePortfolio(
             id = TypedId(),
@@ -33,10 +38,27 @@ constructor(
                 else TypedId(defaultUsers.primaryUser.userId),
             name = "Portfolio-$index")
       }
+  private val stocks: List<String> = listOf("VTI", "VXUS", "VOO")
+  private val sharesOwned: List<SharesOwned> =
+      portfolios.flatMap { portfolio ->
+        stocks.mapIndexed { index, symbol ->
+          SharesOwned(
+              id = TypedId(),
+              portfolioId = portfolio.id,
+              userId = portfolio.userId,
+              dateRangeStart = TODO(),
+              dateRangeEnd = TODO(),
+              symbol = symbol,
+              totalShares = BigDecimal("${index + 1}"))
+        }
+      }
 
   @BeforeEach
   fun setup() {
-    runBlocking { portfolioRepo.createPortfolios(portfolios) }
+    runBlocking {
+      portfolioRepo.createAllPortfolios(portfolios)
+      sharesOwnedRepo.createAllSharesOwned(sharesOwned)
+    }
   }
   @Test
   fun `gets list of portfolio names for user`() {
