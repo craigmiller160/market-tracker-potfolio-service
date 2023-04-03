29f11f5f0eaf288287f81d726cb18932f891b2ed
Craig Miller <craigmiller160@gmail.com>
2023-04-03 16:36:02 -0400

    Disabled test

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/DownloadRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/DownloadRoutesTest.kt
@@ -1,11 +1,13 @@
 package io.craigmiller160.markettracker.portfolio.web.routes
 
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 @MarketTrackerPortfolioIntegrationTest
 class DownloadRoutesTest {
   @Test
+  @Disabled
   fun `downloads the remote data`() {
     TODO()
   }
