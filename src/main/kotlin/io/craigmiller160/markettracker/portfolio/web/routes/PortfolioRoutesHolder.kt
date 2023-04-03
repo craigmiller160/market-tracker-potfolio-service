8f052567e1900eaecf504df20e3ee16f333d6544
Craig Miller <craigmiller160@gmail.com>
2023-04-03 16:59:16 -0400

    Refactored routes

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesHolder.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesHolder.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.markettracker.portfolio.web.routes
 
 import io.craigmiller160.markettracker.portfolio.web.handlers.PortfolioHandler
 import io.craigmiller160.markettracker.portfolio.web.swagger.SwaggerBuilders
+import io.craigmiller160.markettracker.portfolio.web.swagger.SwaggerOperationBuilder
 import io.craigmiller160.markettracker.portfolio.web.swagger.coSwaggerRouter
 import io.craigmiller160.markettracker.portfolio.web.types.PortfolioResponse
 import io.craigmiller160.markettracker.portfolio.web.types.SharesOwnedResponse
@@ -16,59 +17,53 @@ class PortfolioRoutesHolder {
   @Bean
   fun portfolioRoutes(handler: PortfolioHandler): RouterFunction<ServerResponse> = coSwaggerRouter {
     GET("/portfolios", handler::getPortfolios) {
-      it.operationId("getPortfolios")
-          .response(
-              SwaggerBuilders.responseBuilder()
-                  .responseCode("200")
-                  .implementationArray(PortfolioResponse::class.java))
+      it.operationId("getPortfolios").addPortfolioListResponse()
     }
     GET("/portfolios/combined", handler::getStocksForAllPortfoliosCombined) {
-      it.operationId("getStocksForAllPortfoliosCombined")
-          .response(
-              SwaggerBuilders.responseBuilder()
-                  .responseCode("200")
-                  .implementationArray(String::class.java))
+      it.operationId("getStocksForAllPortfoliosCombined").addStockListResponse()
     }
     GET(
         "/portfolios/combined/{stockSymbol}/shares",
         handler::getSharesOwnedForAllPortfoliosCombinedStock) {
           it.operationId("getSharesOwnedForAllPortfoliosCombinedStock")
-              .parameter(
-                  SwaggerBuilders.pathParamBuilder()
-                      .name("stockSymbol")
-                      .implementation(String::class.java))
-              .response(
-                  SwaggerBuilders.responseBuilder()
-                      .responseCode("200")
-                      .implementationArray(SharesOwnedResponse::class.java))
+              .addStockSymbolParameter()
+              .addSharesOwnedResponse()
         }
     GET("/portfolios/{portfolioId}", handler::getStocksForPortfolio) {
-      it.operationId("getStocksForPortfolio")
-          .parameter(
-              SwaggerBuilders.pathParamBuilder()
-                  .name("portfolioId")
-                  .implementation(UUID::class.java))
-          .response(
-              SwaggerBuilders.responseBuilder()
-                  .responseCode("200")
-                  .implementationArray(String::class.java))
+      it.operationId("getStocksForPortfolio").addPortfolioIdParameter().addStockListResponse()
     }
     GET(
         "/portfolios/{portfolioId}/{stockSymbol}/shares",
         handler::getSharesOwnedForPortfolioStock) {
           it.operationId("getSharesOwnedForPortfolioStock")
-              .parameter(
-                  SwaggerBuilders.pathParamBuilder()
-                      .name("portfolioId")
-                      .implementation(UUID::class.java))
-              .parameter(
-                  SwaggerBuilders.pathParamBuilder()
-                      .name("stockSymbol")
-                      .implementation(String::class.java))
-              .response(
-                  SwaggerBuilders.responseBuilder()
-                      .responseCode("200")
-                      .implementationArray(SharesOwnedResponse::class.java))
+              .addPortfolioIdParameter()
+              .addStockSymbolParameter()
+              .addSharesOwnedResponse()
         }
   }
+
+  private fun SwaggerOperationBuilder.addPortfolioListResponse(): SwaggerOperationBuilder =
+      response(
+          SwaggerBuilders.responseBuilder()
+              .responseCode("200")
+              .implementationArray(PortfolioResponse::class.java))
+
+  private fun SwaggerOperationBuilder.addStockListResponse(): SwaggerOperationBuilder =
+      response(
+          SwaggerBuilders.responseBuilder()
+              .responseCode("200")
+              .implementationArray(String::class.java))
+  private fun SwaggerOperationBuilder.addPortfolioIdParameter(): SwaggerOperationBuilder =
+      parameter(
+          SwaggerBuilders.pathParamBuilder().name("portfolioId").implementation(UUID::class.java))
+
+  private fun SwaggerOperationBuilder.addStockSymbolParameter(): SwaggerOperationBuilder =
+      parameter(
+          SwaggerBuilders.pathParamBuilder().name("stockSymbol").implementation(String::class.java))
+
+  private fun SwaggerOperationBuilder.addSharesOwnedResponse(): SwaggerOperationBuilder =
+      response(
+          SwaggerBuilders.responseBuilder()
+              .responseCode("200")
+              .implementationArray(SharesOwnedResponse::class.java))
 }
