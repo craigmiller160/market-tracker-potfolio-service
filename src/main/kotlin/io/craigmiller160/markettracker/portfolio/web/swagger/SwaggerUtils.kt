3e81bfab63bee8b78f7d6bb34bd6af6dc779d901
Craig Miller <craigmiller160@gmail.com>
2023-04-04 11:36:13 -0400

    More refactoring of swagger utilities

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/swagger/SwaggerUtils.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/swagger/SwaggerUtils.kt
@@ -2,6 +2,8 @@ package io.craigmiller160.markettracker.portfolio.web.swagger
 
 import io.craigmiller160.markettracker.portfolio.web.routes.RoutesHolder
 import io.craigmiller160.markettracker.portfolio.web.types.PortfolioResponse
+import io.craigmiller160.markettracker.portfolio.web.types.SharesOwnedResponse
+import java.util.UUID
 
 context(RoutesHolder)
 
@@ -10,3 +12,31 @@ fun SwaggerOperationBuilder.addPortfolioListResponse(): SwaggerOperationBuilder 
         SwaggerBuilders.responseBuilder()
             .responseCode("200")
             .implementationArray(PortfolioResponse::class.java))
+
+context(RoutesHolder)
+
+fun SwaggerOperationBuilder.addStockListResponse(): SwaggerOperationBuilder =
+    response(
+        SwaggerBuilders.responseBuilder()
+            .responseCode("200")
+            .implementationArray(String::class.java))
+
+context(RoutesHolder)
+
+fun SwaggerOperationBuilder.addPortfolioIdParameter(): SwaggerOperationBuilder =
+    parameter(
+        SwaggerBuilders.pathParamBuilder().name("portfolioId").implementation(UUID::class.java))
+
+context(RoutesHolder)
+
+fun SwaggerOperationBuilder.addStockSymbolParameter(): SwaggerOperationBuilder =
+    parameter(
+        SwaggerBuilders.pathParamBuilder().name("stockSymbol").implementation(String::class.java))
+
+context(RoutesHolder)
+
+fun SwaggerOperationBuilder.addSharesOwnedResponse(): SwaggerOperationBuilder =
+    response(
+        SwaggerBuilders.responseBuilder()
+            .responseCode("200")
+            .implementationArray(SharesOwnedResponse::class.java))
