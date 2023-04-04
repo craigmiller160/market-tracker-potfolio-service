17c89711253f4a6ea2e21816438c6ffcf93fc959
Craig Miller <craigmiller160@gmail.com>
2023-04-04 19:10:49 -0400

    Integrated new params into swagger

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesHolder.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesHolder.kt
@@ -1,9 +1,12 @@
 package io.craigmiller160.markettracker.portfolio.web.routes
 
 import io.craigmiller160.markettracker.portfolio.web.handlers.PortfolioHandler
+import io.craigmiller160.markettracker.portfolio.web.swagger.addEndDateParameter
+import io.craigmiller160.markettracker.portfolio.web.swagger.addIntervalParameter
 import io.craigmiller160.markettracker.portfolio.web.swagger.addPortfolioIdParameter
 import io.craigmiller160.markettracker.portfolio.web.swagger.addPortfolioListResponse
 import io.craigmiller160.markettracker.portfolio.web.swagger.addSharesOwnedResponse
+import io.craigmiller160.markettracker.portfolio.web.swagger.addStartDateParameter
 import io.craigmiller160.markettracker.portfolio.web.swagger.addStockListResponse
 import io.craigmiller160.markettracker.portfolio.web.swagger.addStockSymbolParameter
 import io.craigmiller160.markettracker.portfolio.web.swagger.coSwaggerRouter
@@ -27,6 +30,9 @@ class PortfolioRoutesHolder {
         handler::getSharesOwnedForAllPortfoliosCombinedStock) {
           it.operationId("getSharesOwnedForAllPortfoliosCombinedStock")
               .addStockSymbolParameter()
+              .addStartDateParameter()
+              .addEndDateParameter()
+              .addIntervalParameter()
               .addSharesOwnedResponse()
         }
     GET("/portfolios/{portfolioId}", handler::getStocksForPortfolio) {
@@ -38,6 +44,9 @@ class PortfolioRoutesHolder {
           it.operationId("getSharesOwnedForPortfolioStock")
               .addPortfolioIdParameter()
               .addStockSymbolParameter()
+              .addStartDateParameter()
+              .addEndDateParameter()
+              .addIntervalParameter()
               .addSharesOwnedResponse()
         }
   }
