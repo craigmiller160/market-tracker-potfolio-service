c3d956eb173998b913837a21c03afbfa17f6faf5
Craig Miller <craigmiller160@gmail.com>
2023-04-06 14:52:24 -0400

    Updated with TODOs

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwnedInterval.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwnedInterval.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.markettracker.portfolio.domain.models
 
 enum class SharesOwnedInterval {
+  // TODO do I need this one? Maybe single would be better?
   MINUTELY,
   DAILY,
   WEEKLY,
