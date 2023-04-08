67399d06b72cd2fcb4f3752221649f320ff1fda2
Craig Miller <craigmiller160@gmail.com>
2023-04-07 23:05:49 -0400

    Updated sql

--- src/main/resources/sql/sharesOwned/getSharesOwnedAtInterval.sql
+++ src/main/resources/sql/sharesOwned/getSharesOwnedAtInterval.sql
@@ -2,7 +2,10 @@ WITH the_dates AS (
     SELECT date_trunc('day', dd)::date AS the_date
     FROM generate_series(:startDate::date, :endDate::date, :interval::interval) dd
 )
-SELECT td.the_date,
+SELECT td.the_date, so.user_id, so.symbol, so.total_shares
+{{#portfolioId}}
+so.portfolio_id
+{{/portfolioId}}
 FROM the_dates td
 JOIN shares_owned so ON td.the_date <@ so.date_range
 WHERE so.user_id = :userId
