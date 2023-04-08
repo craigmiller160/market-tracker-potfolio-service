c2ecf941e510ced2254fd8bfdd062916d558eb04
Craig Miller <craigmiller160@gmail.com>
2023-04-07 22:39:49 -0400

    Refactored sql template handling

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
 import arrow.core.Either
+import arrow.core.flatMap
 import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
@@ -67,29 +68,35 @@ class DatabaseClientSharesOwnedRepository(
       userId: TypedId<UserId>,
       portfolioId: TypedId<PortfolioId>
   ): TryEither<List<String>> =
-      sqlLoader.loadSql(FIND_UNIQUE_STOCKS_SQL).mapCatch { sql ->
-        databaseClient
-            .sql(sql)
-            .bind("userId", userId.value)
-            .bind("portfolioId", portfolioId.value)
-            .map { row -> row.get("symbol")?.toString() }
-            .all()
-            .toIterable()
-            .toList()
-            .filterNotNull()
-      }
+      sqlLoader
+          .loadSqlMustacheTemplate(FIND_UNIQUE_STOCKS_SQL)
+          .flatMap { template -> template.executeWithSectionsEnabled("portfolioId") }
+          .mapCatch { sql ->
+            databaseClient
+                .sql(sql)
+                .bind("userId", userId.value)
+                .bind("portfolioId", portfolioId.value)
+                .map { row -> row.get("symbol")?.toString() }
+                .all()
+                .toIterable()
+                .toList()
+                .filterNotNull()
+          }
 
   override suspend fun findUniqueStocksForUser(userId: TypedId<UserId>): TryEither<List<String>> =
-      sqlLoader.loadSql(FIND_UNIQUE_STOCKS_FOR_USER_SQL).mapCatch { sql ->
-        databaseClient
-            .sql(sql)
-            .bind("userId", userId.value)
-            .map { row -> row.get("symbol")?.toString() }
-            .all()
-            .toIterable()
-            .toList()
-            .filterNotNull()
-      }
+      sqlLoader
+          .loadSqlMustacheTemplate(FIND_UNIQUE_STOCKS_SQL)
+          .flatMap { template -> template.executeWithSectionsEnabled() }
+          .mapCatch { sql ->
+            databaseClient
+                .sql(sql)
+                .bind("userId", userId.value)
+                .map { row -> row.get("symbol")?.toString() }
+                .all()
+                .toIterable()
+                .toList()
+                .filterNotNull()
+          }
 
   override suspend fun getSharesOwnedAtIntervalInPortfolio(
       userId: TypedId<UserId>,
