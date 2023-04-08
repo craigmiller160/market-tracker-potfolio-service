7912c3ddf87fcdc56cc6e153330015daa8be4a5b
Craig Miller <craigmiller160@gmail.com>
2023-04-08 17:56:36 -0400

    Reverting hacks

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepository.kt
@@ -15,7 +15,6 @@ import io.craigmiller160.markettracker.portfolio.extensions.mapCatch
 import io.craigmiller160.markettracker.portfolio.extensions.toSqlBatches
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactor.flux
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.stereotype.Repository
 import reactor.kotlin.core.publisher.toFlux
@@ -36,15 +35,6 @@ class DatabaseClientSharesOwnedRepository(
         sharesOwned
       }
 
-  // TODo delete this
-  private fun filter(list: List<SharesOwned>): List<SharesOwned> {
-    val start = 0
-    val targetEnd = 10
-    val actualEnd = if (list.size - 1 >= targetEnd) targetEnd else list.size - 1
-
-    return list.filter { it.symbol == "SPYG" }
-  }
-
   private suspend fun createAsBatch(
       sharesOwned: List<SharesOwned>
   ): suspend (String) -> TryEither<List<Long>> = { sql ->
@@ -52,24 +42,19 @@ class DatabaseClientSharesOwnedRepository(
       databaseClient
           .inConnectionMany { conn ->
             val statement = conn.createStatement(sql)
-            val filtered = sharesOwned.let(this::filter) // TODO delete this
-            if (filtered.isNotEmpty()) {
-              filtered
-                  .toSqlBatches(statement) { record, stmt ->
-                    stmt
-                        .bind(0, record.id.value)
-                        .bind(1, record.userId.value)
-                        .bind(2, record.portfolioId.value)
-                        .bind(3, record.dateRange)
-                        .bind(4, record.symbol)
-                        .bind(5, record.totalShares)
-                  }
-                  .execute()
-                  .toFlux()
-                  .flatMap { result -> result.rowsUpdated.toFlux() }
-            } else {
-              flux { 1L }
-            }
+            sharesOwned
+                .toSqlBatches(statement) { record, stmt ->
+                  stmt
+                      .bind(0, record.id.value)
+                      .bind(1, record.userId.value)
+                      .bind(2, record.portfolioId.value)
+                      .bind(3, record.dateRange)
+                      .bind(4, record.symbol)
+                      .bind(5, record.totalShares)
+                }
+                .execute()
+                .toFlux()
+                .flatMap { result -> result.rowsUpdated.toFlux() }
           }
           .asFlow()
           .toList()
