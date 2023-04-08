274dc890008fa212baa1670ea0966825c7c74b9c
Craig Miller <craigmiller160@gmail.com>
2023-04-07 23:19:07 -0400

    More refactoring row mapper tests to be re-usable

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedOnDateRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/SharesOwnedOnDateRowMapper.kt
@@ -1,1 +1,5 @@
 package io.craigmiller160.markettracker.portfolio.domain.rowmappers
+
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwnedOnDate
+
+val sharesOwnedOnDateRowMapper: RowMapper<SharesOwnedOnDate> = { row, _ -> TODO() }
--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapperTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/PortfolioRowMapperTest.kt
@@ -4,24 +4,15 @@ import arrow.core.Either
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
-import io.craigmiller160.markettracker.portfolio.extensions.TryEither
-import io.kotest.assertions.arrow.core.shouldBeLeft
-import io.kotest.assertions.arrow.core.shouldBeRight
-import io.kotest.matchers.nulls.shouldNotBeNull
-import io.kotest.matchers.shouldBe
-import io.mockk.mockk
-import io.r2dbc.spi.RowMetadata
 import java.util.UUID
 import java.util.stream.Stream
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 
-private typealias PortfolioData = Pair<Map<String, Any?>, TryEither<Portfolio>>
-
 class PortfolioRowMapperTest {
   companion object {
     @JvmStatic
-    fun portfolioData(): Stream<PortfolioData> {
+    fun portfolioData(): Stream<RowMapperData<Portfolio>> {
       val id = UUID.randomUUID()
       val userId = UUID.randomUUID()
       val name = "Bob"
@@ -38,24 +29,9 @@ class PortfolioRowMapperTest {
     }
   }
 
-  private val metadata: RowMetadata = mockk()
-
   @ParameterizedTest
   @MethodSource("portfolioData")
-  fun `maps Portfolio from row`(data: PortfolioData) {
-    val (map, expected) = data
-    val row = MockRow(map)
-    val actual = portfolioRowMapper(row, metadata)
-    when (expected) {
-      is Either.Right -> actual.shouldBeRight(expected.value)
-      is Either.Left -> {
-        val actualException = actual.shouldBeLeft(expected.value)
-        expected.value.cause?.let { expectedCause ->
-          val actualCause = actualException.cause.shouldNotBeNull()
-          actualCause.javaClass.shouldBe(expectedCause.javaClass)
-          actualCause.message.shouldBe(expectedCause.message)
-        }
-      }
-    }
+  fun `maps Portfolio from row`(data: RowMapperData<Portfolio>) {
+    validateRowMapper(portfolioRowMapper, data)
   }
 }
