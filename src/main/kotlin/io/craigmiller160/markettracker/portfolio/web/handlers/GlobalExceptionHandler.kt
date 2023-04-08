6ca8b195d367fffced8531b59d5f8a8dc43b2182
Craig Miller <craigmiller160@gmail.com>
2023-04-08 14:09:09 -0400

    More refactoring and transferring changes

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/GlobalExceptionHandler.kt
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.server.WebExceptionHandler
 import reactor.core.publisher.Mono
 
@@ -21,12 +22,12 @@ class GlobalExceptionHandler(private val objectMapper: ObjectMapper) {
   fun exceptionHandler(): WebExceptionHandler = WebExceptionHandler { exchange, exception ->
     log.error(exception.message, exception)
 
-    val status = HttpStatus.INTERNAL_SERVER_ERROR
+    val (status, message) = getStatusAndMessageFromException(exception)
     val response =
         ErrorResponse(
             method = exchange.request.method.name(),
             uri = exchange.request.uri.toString(),
-            message = "",
+            message = message,
             status = status.value())
 
     Either.catch { objectMapper.writeValueAsBytes(response) }
@@ -44,3 +45,11 @@ class GlobalExceptionHandler(private val objectMapper: ObjectMapper) {
         }
   }
 }
+
+private data class StatusAndMessage(val status: HttpStatus, val message: String)
+
+private fun getStatusAndMessageFromException(ex: Throwable): StatusAndMessage =
+    ex.javaClass.getAnnotation(ResponseStatus::class.java)?.let { annotation ->
+      StatusAndMessage(status = annotation.code, message = "${annotation.reason}: ${ex.message}")
+    }
+        ?: StatusAndMessage(status = HttpStatus.INTERNAL_SERVER_ERROR, message = "")
--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcontainer/ExceptionThrowingController.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/testcontainer/ExceptionThrowingController.kt
@@ -1,5 +1,7 @@
 package io.craigmiller160.markettracker.portfolio.testcontainer
 
+import com.github.dockerjava.api.exception.BadRequestException
+import io.craigmiller160.markettracker.portfolio.web.exceptions.MissingParameterException
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,5 +12,15 @@ class ExceptionThrowingController {
   @GetMapping("/runtime-exception")
   fun runtimeException() {
     throw RuntimeException("Dying")
+  }
+
+  @GetMapping("/missing-parameter")
+  fun missingParameter() {
+    throw MissingParameterException("stuff")
+  }
+
+  @GetMapping("/bad-request")
+  fun badRequest() {
+    throw BadRequestException("Dying")
   }
 }
