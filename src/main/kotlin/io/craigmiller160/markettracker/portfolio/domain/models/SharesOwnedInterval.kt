a3f9aac303450d1d4d2b5be61d3fb60f69d2cc31
Craig Miller <craigmiller160@gmail.com>
2023-04-07 23:32:42 -0400

    Binding all the variables

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwnedInterval.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/models/SharesOwnedInterval.kt
@@ -1,8 +1,8 @@
 package io.craigmiller160.markettracker.portfolio.domain.models
 
-enum class SharesOwnedInterval {
-  SINGLE,
-  DAILY,
-  WEEKLY,
-  MONTHLY
+enum class SharesOwnedInterval(val sql: String) {
+  SINGLE("1 day"),
+  DAILY("1 day"),
+  WEEKLY("1 week"),
+  MONTHLY("1 month")
 }
