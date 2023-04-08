1bd012f3b3c4309d92ea51ec2b8281a603a7f562
Craig Miller <craigmiller160@gmail.com>
2023-04-08 14:15:11 -0400

    More refactoring

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/web/routes/PortfolioRoutesTest.kt
@@ -1,19 +1,14 @@
 package io.craigmiller160.markettracker.portfolio.web.routes
 
+import arrow.core.flatMap
+import arrow.core.getOrElse
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
-import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
-import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
-import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.domain.models.toPortfolioResponse
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
 import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.craigmiller160.markettracker.portfolio.testutils.DefaultUsers
-import java.math.BigDecimal
-import java.time.LocalDate
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -32,51 +27,19 @@ constructor(
     private const val DATE_RANGE_LENGTH = 10L
   }
 
-  private val portfolios: List<Portfolio> =
-      (0 until 5).map { index ->
-        BasePortfolio(
-            id = TypedId(),
-            userId =
-                if (index == 0) TypedId(defaultUsers.secondaryUser.userId)
-                else TypedId(defaultUsers.primaryUser.userId),
-            name = "Portfolio-$index")
-      }
-  private val stocks: List<String> = listOf("VTI", "VXUS", "VOO")
-  private val baseDate = LocalDate.now()
-  private val sharesOwned: List<SharesOwned> =
-      portfolios.flatMapIndexed { portfolioIndex, portfolio ->
-        val stocks =
-            stocks.map { stock ->
-              if (portfolioIndex % 2 == 0) {
-                stock
-              } else {
-                "$stock-$portfolioIndex"
-              }
-            }
-
-        stocks.mapIndexed { stockIndex, symbol ->
-          val dateOffset = DATE_RANGE_LENGTH * stockIndex
-          SharesOwned(
-              id = TypedId(),
-              portfolioId = portfolio.id,
-              userId = portfolio.userId,
-              dateRangeStart = baseDate.plusDays(dateOffset),
-              dateRangeEnd = baseDate.plusDays(dateOffset + DATE_RANGE_LENGTH),
-              symbol = symbol,
-              totalShares = BigDecimal("${stockIndex + 1}"))
-        }
-      }
-
-  @BeforeEach
-  fun setup() {
-    runBlocking {
-      portfolioRepo.createAllPortfolios(portfolios)
-      sharesOwnedRepo.createAllSharesOwned(sharesOwned)
+  private fun createData(offsetDays: Int, numRecords: Int): PortfolioRouteData = runBlocking {
+    createPortfolioRouteData(defaultUsers, offsetDays, numRecords).let { data ->
+      portfolioRepo
+          .createAllPortfolios(data.portfolios)
+          .flatMap { sharesOwnedRepo.createAllSharesOwned(data.sharesOwned) }
+          .map { data }
+          .getOrElse { throw it }
     }
   }
   @Test
   fun `gets list of portfolio names for user`() {
-    val expectedResponse = portfolios.drop(1).map { it.toPortfolioResponse() }
+    val data = createData(10, 100)
+    val expectedResponse = data.portfolios.drop(1).map { it.toPortfolioResponse() }
     webTestClient
         .get()
         .uri("/portfolios")
@@ -90,10 +53,11 @@ constructor(
 
   @Test
   fun `gets list of stocks in portfolio for user`() {
-    val expectedResponse = stocks.map { "$it-1" }.sorted()
+    val data = createData(10, 100)
+    val expectedResponse = data.uniqueStocks.map { "$it-1" }.sorted()
     webTestClient
         .get()
-        .uri("/portfolios/${portfolios[1].id}")
+        .uri("/portfolios/$data.{portfolios[1].id}")
         .header("Authorization", "Bearer ${defaultUsers.primaryUser.token}")
         .exchange()
         .expectStatus()
@@ -104,9 +68,10 @@ constructor(
 
   @Test
   fun `gets list of stocks in portfolio that the user does not own`() {
+    val data = createData(10, 100)
     webTestClient
         .get()
-        .uri("/portfolios/${portfolios[0].id}")
+        .uri("/portfolios/${data.portfolios[0].id}")
         .header("Authorization", "Bearer ${defaultUsers.primaryUser.token}")
         .exchange()
         .expectStatus()
@@ -117,8 +82,9 @@ constructor(
 
   @Test
   fun `gets a list of unique stocks for all portfolios combined`() {
+    val data = createData(10, 100)
     val expectedResponse =
-        stocks
+        data.uniqueStocks
             .flatMap { stock ->
               (1..4).map { index ->
                 if (index % 2 == 0) {
