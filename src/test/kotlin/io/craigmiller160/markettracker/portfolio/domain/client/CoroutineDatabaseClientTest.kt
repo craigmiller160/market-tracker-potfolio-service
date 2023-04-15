bea3bb54968b221e4f70791b7db655adf7a178dd
Craig Miller <craigmiller160@gmail.com>
2023-04-15 12:11:58 -0400

    More work setting up tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
@@ -3,11 +3,15 @@ package io.craigmiller160.markettracker.portfolio.domain.client
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.r2dbc.core.DatabaseClient
 
 @MarketTrackerPortfolioIntegrationTest
 class CoroutineDatabaseClientTest
 @Autowired
-constructor(private val client: CoroutineDatabaseClient) {
+constructor(
+    private val coroutineClient: CoroutineDatabaseClient,
+    private val client: DatabaseClient
+) {
   @Test
   fun `query without params`() {
     TODO()
