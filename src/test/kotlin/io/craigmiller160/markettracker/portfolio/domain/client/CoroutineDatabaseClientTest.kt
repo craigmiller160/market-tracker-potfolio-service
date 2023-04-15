ccc2407a9ce669aeaecbcdd47e4bd01ef1c612c8
Craig Miller <craigmiller160@gmail.com>
2023-04-15 12:10:38 -0400

    Stubbed out tests

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
@@ -0,0 +1,37 @@
+package io.craigmiller160.markettracker.portfolio.domain.client
+
+import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import org.junit.jupiter.api.Test
+
+@MarketTrackerPortfolioIntegrationTest
+class CoroutineDatabaseClientTest {
+  @Test
+  fun `query without params`() {
+    TODO()
+  }
+
+  @Test
+  fun `query with params`() {
+    TODO()
+  }
+
+  @Test
+  fun `update without params`() {
+    TODO()
+  }
+
+  @Test
+  fun `update with params`() {
+    TODO()
+  }
+
+  @Test
+  fun `batch update without params`() {
+    TODO()
+  }
+
+  @Test
+  fun `batch update with params`() {
+    TODO()
+  }
+}
