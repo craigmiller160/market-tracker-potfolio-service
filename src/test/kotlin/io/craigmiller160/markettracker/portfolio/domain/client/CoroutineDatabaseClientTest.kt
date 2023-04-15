65bb3229fde68860a68cb9f188aadd3cd7ae3f06
Craig Miller <craigmiller160@gmail.com>
2023-04-15 12:21:16 -0400

    Implemented some tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
@@ -1,6 +1,12 @@
 package io.craigmiller160.markettracker.portfolio.domain.client
 
+import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
+import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
+import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import io.kotest.assertions.arrow.core.shouldBeRight
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.r2dbc.core.DatabaseClient
@@ -14,12 +20,33 @@ constructor(
 ) {
   @Test
   fun `query without params`() {
-    TODO()
+    runBlocking {
+      insertPortfolio("abc")
+      insertPortfolio("def")
+    }
+
+    val result = runBlocking {
+      coroutineClient.query("SELECT COUNT(*) AS the_count FROM portfolios").map { list ->
+        list.first().get("count")
+      }
+    }
+    result.shouldBeRight(2L)
   }
 
   @Test
   fun `query with params`() {
-    TODO()
+    val expectedId = runBlocking {
+      val first = insertPortfolio("abc")
+      insertPortfolio("def")
+      first.id.value
+    }
+
+    val result = runBlocking {
+      coroutineClient
+          .query("SELECT id FROM portfolios WHERE name = :name", mapOf("name" to "abc"))
+          .map { list -> list.map { it.get("id") } }
+    }
+    result.shouldBeRight(listOf(expectedId))
   }
 
   @Test
@@ -40,5 +67,18 @@ constructor(
   @Test
   fun `batch update with params`() {
     TODO()
+  }
+
+  private suspend fun insertPortfolio(name: String): Portfolio {
+    val portfolio = BasePortfolio(id = TypedId(), userId = TypedId(), name = name)
+    client
+        .sql("INSERT INTO portfolios (id, user_id, name) VALUES (:id, :userId, :name)")
+        .bind("id", portfolio.id.value)
+        .bind("userId", portfolio.userId.value)
+        .bind("name", portfolio.name)
+        .fetch()
+        .rowsUpdated()
+        .awaitSingle()
+    return portfolio
   }
 }
