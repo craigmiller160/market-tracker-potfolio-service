eb0c6ac00ada5ccf3864dac871190e4eb98ec03c
Craig Miller <craigmiller160@gmail.com>
2023-04-15 12:05:29 -0400

    Trying and so far failing to make TypedId an inline class

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/common/typedid/TypedId.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/common/typedid/TypedId.kt
@@ -3,13 +3,11 @@ package io.craigmiller160.markettracker.portfolio.common.typedid
 import io.swagger.v3.oas.annotations.media.Schema
 import java.util.UUID
 
+@JvmInline
 @Schema(implementation = UUID::class)
-class TypedId<T>(val value: UUID = UUID.randomUUID()) : Comparable<TypedId<T>> {
+value class TypedId<T>(val value: UUID = UUID.randomUUID()) : Comparable<TypedId<T>> {
   constructor() : this(UUID.randomUUID())
   constructor(value: String) : this(UUID.fromString(value))
 
   override fun compareTo(other: TypedId<T>): Int = value.compareTo(other.value)
-  override fun equals(other: Any?): Boolean = other is TypedId<*> && value == other.value
-  override fun hashCode(): Int = value.hashCode()
-  override fun toString(): String = value.toString()
 }
