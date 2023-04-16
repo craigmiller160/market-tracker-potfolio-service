20a145b1c98f53fdbff46e309cd5c01d36b0b01c
Craig Miller <craigmiller160@gmail.com>
2023-04-16 12:53:10 -0400

    More tests

---
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/RowTest.kt
@@ -0,0 +1,35 @@
+package io.craigmiller160.markettracker.portfolio.domain.client
+
+import org.junit.jupiter.api.Test
+
+class RowTest {
+  @Test
+  fun `get optional, not null, with valid type`() {
+    TODO()
+  }
+
+  @Test
+  fun `get optional, not null, with invalid type`() {
+    TODO()
+  }
+
+  @Test
+  fun `get optional, is null`() {
+    TODO()
+  }
+
+  @Test
+  fun `get required, not null, with valid type`() {
+    TODO()
+  }
+
+  @Test
+  fun `get required, not null, with invalid type`() {
+    TODO()
+  }
+
+  @Test
+  fun `get required, is null`() {
+    TODO()
+  }
+}
