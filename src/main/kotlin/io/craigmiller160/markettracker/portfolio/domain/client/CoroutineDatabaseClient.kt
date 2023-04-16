e716f90b21feb8730d1217279feca0ca448ed44b
Craig Miller <craigmiller160@gmail.com>
2023-04-16 10:16:52 -0400

    Making progress

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClient.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClient.kt
@@ -1,12 +1,24 @@
 package io.craigmiller160.markettracker.portfolio.domain.client
 
+import arrow.core.fold
+import arrow.typeclasses.Monoid
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.r2dbc.spi.Row
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.DatabaseClient.GenericExecuteSpec
 
 class CoroutineDatabaseClient(private val databaseClient: DatabaseClient) {
   suspend fun query(sql: String, params: Map<String, Any> = mapOf()): TryEither<List<Row>> {
-    TODO()
+    databaseClient
+        .sql(sql)
+        .let { executeSpec ->
+          params.entries
+              .map { (key, value) -> { spec: GenericExecuteSpec -> spec.bind(key, value) } }
+              .fold(executeSpecBinderMonoid)
+              .let { fn -> fn(executeSpec) }
+        }
+        .fetch()
+        .all()
   }
 
   suspend fun update(sql: String, params: Map<String, Any> = mapOf()): TryEither<Long> {
@@ -20,3 +32,15 @@ class CoroutineDatabaseClient(private val databaseClient: DatabaseClient) {
     TODO()
   }
 }
+
+private typealias ExecuteSpecBinder = (GenericExecuteSpec) -> GenericExecuteSpec
+
+private val executeSpecBinderMonoid =
+    object : Monoid<ExecuteSpecBinder> {
+      override fun empty(): ExecuteSpecBinder = { it }
+
+      override fun ExecuteSpecBinder.combine(b: ExecuteSpecBinder): ExecuteSpecBinder =
+          { executeSpec ->
+            this(executeSpec).let { b(it) }
+          }
+    }
