501aedcd9b11754cdd702a10a042a07f3716df8b
Craig Miller <craigmiller160@gmail.com>
2023-04-16 13:06:14 -0400

    Setting up test values

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/RowTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/RowTest.kt
@@ -1,8 +1,13 @@
 package io.craigmiller160.markettracker.portfolio.domain.client
 
+import arrow.core.Either
+import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
+import java.lang.NullPointerException
 import java.util.UUID
+import java.util.stream.Stream
+import kotlin.reflect.KClass
 import org.junit.jupiter.api.Test
 
 class RowTest {
@@ -10,6 +15,20 @@ class RowTest {
     private const val KEY = "hello"
     private val VALUE = UUID.randomUUID()
     private val VALUE_TYPE = UUID::class
+
+    @JvmStatic
+    fun getOptionalValues(): Stream<RowValues> =
+        Stream.of(
+            RowValues(KEY, VALUE, VALUE_TYPE, Either.Right(VALUE)),
+            RowValues(KEY, VALUE, Int::class, classCastLeft(Int::class)),
+            RowValues(KEY, null, Int::class, Either.Right(null)))
+
+    @JvmStatic
+    fun getRequiredValues(): Stream<RowValues> =
+        Stream.of(
+            RowValues(KEY, VALUE, VALUE_TYPE, Either.Right(VALUE)),
+            RowValues(KEY, VALUE, Int::class, classCastLeft(Int::class)),
+            RowValues(KEY, VALUE, Int::class, nullLeft(KEY)))
   }
   @Test
   fun `get optional, not null, with valid type`() {
@@ -49,3 +68,16 @@ class RowTest {
     TODO()
   }
 }
+
+data class RowValues(
+    val key: String,
+    val value: Any?,
+    val valueType: KClass<*>,
+    val expected: TryEither<*>
+)
+
+private fun classCastLeft(type: KClass<*>): TryEither<*> =
+    Either.Left(ClassCastException("Value cannot be cast to ${type.qualifiedName}"))
+
+private fun nullLeft(name: String): TryEither<*> =
+    Either.Left(NullPointerException("Value is null: Missing $name column"))
