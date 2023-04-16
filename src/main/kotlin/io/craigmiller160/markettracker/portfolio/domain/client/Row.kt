325531d8686651411fb24800c82dd7f7547064f4
Craig Miller <craigmiller160@gmail.com>
2023-04-16 12:43:50 -0400

    Implemented row

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/Row.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/Row.kt
@@ -2,11 +2,13 @@ package io.craigmiller160.markettracker.portfolio.domain.client
 
 import arrow.core.Either
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
+import io.craigmiller160.markettracker.portfolio.extensions.leftIfNull
 import kotlin.reflect.KClass
 import kotlin.reflect.cast
 
-class Row(private val data: Map<String, Any?>) {
-  operator fun get(key: String): Any? = data[key]
-  operator fun <T : Any> get(key: String, type: KClass<T>): TryEither<T>? =
-      data[key]?.let { Either.catch { type.cast(it) } }
+data class Row(private val data: Map<String, Any?>) {
+  fun <T : Any> getOptional(key: String, type: KClass<T>): TryEither<T?> =
+      data[key]?.let { Either.catch { type.cast(it) } } ?: Either.Right(null)
+  fun <T : Any> getRequired(key: String, type: KClass<T>): TryEither<Any> =
+      data[key]?.let { Either.catch { type.cast(it) } }.leftIfNull("Missing $key column")
 }
