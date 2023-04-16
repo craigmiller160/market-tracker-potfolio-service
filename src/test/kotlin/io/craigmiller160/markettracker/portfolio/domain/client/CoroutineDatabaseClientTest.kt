c57f50fb91395b2f8a6522846bdac732d2c52b3c
Craig Miller <craigmiller160@gmail.com>
2023-04-16 10:25:09 -0400

    Updated tests

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClientTest.kt
@@ -5,12 +5,14 @@ import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.domain.models.Portfolio
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
 import io.kotest.assertions.arrow.core.shouldBeRight
+import io.kotest.matchers.shouldBe
 import java.util.UUID
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.r2dbc.core.DatabaseClient
+import org.springframework.r2dbc.core.awaitSingle
 
 @MarketTrackerPortfolioIntegrationTest
 class CoroutineDatabaseClientTest
@@ -83,6 +85,11 @@ constructor(
           "INSERT INTO portfolios (id, user_id, name) VALUES (:id, :userId, :name)")
     }
     result.shouldBeRight(listOf())
+
+    val count = runBlocking {
+      client.sql("SELECT COUNT(*) AS the_count FROM portfolios").fetch().awaitSingle()["the_count"]
+    }
+    count.shouldBe(0L)
   }
 
   @Test
@@ -97,6 +104,11 @@ constructor(
           "INSERT INTO portfolios (id, user_id, name) VALUES (:id, :userId, :name)", params)
     }
     result.shouldBeRight(listOf(1L, 1L, 1L))
+
+    val count = runBlocking {
+      client.sql("SELECT COUNT(*) AS the_count FROM portfolios").fetch().awaitSingle()["the_count"]
+    }
+    count.shouldBe(3L)
   }
 
   private suspend fun insertPortfolio(name: String): Portfolio {
