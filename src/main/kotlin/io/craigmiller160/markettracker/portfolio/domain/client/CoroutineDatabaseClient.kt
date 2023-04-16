39cfe63eeaf3b5d07c777991185031104184b62f
Craig Miller <craigmiller160@gmail.com>
2023-04-16 10:19:29 -0400

    Implemented first method

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClient.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/CoroutineDatabaseClient.kt
@@ -1,25 +1,33 @@
 package io.craigmiller160.markettracker.portfolio.domain.client
 
+import arrow.core.Either
 import arrow.core.fold
 import arrow.typeclasses.Monoid
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
-import io.r2dbc.spi.Row
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
 import org.springframework.r2dbc.core.DatabaseClient
 import org.springframework.r2dbc.core.DatabaseClient.GenericExecuteSpec
 
 class CoroutineDatabaseClient(private val databaseClient: DatabaseClient) {
-  suspend fun query(sql: String, params: Map<String, Any> = mapOf()): TryEither<List<Row>> {
-    databaseClient
-        .sql(sql)
-        .let { executeSpec ->
-          params.entries
-              .map { (key, value) -> { spec: GenericExecuteSpec -> spec.bind(key, value) } }
-              .fold(executeSpecBinderMonoid)
-              .let { fn -> fn(executeSpec) }
-        }
-        .fetch()
-        .all()
-  }
+  suspend fun query(
+      sql: String,
+      params: Map<String, Any> = mapOf()
+  ): TryEither<List<Map<String, Any>>> =
+      Either.catch {
+        databaseClient
+            .sql(sql)
+            .let { executeSpec ->
+              params.entries
+                  .map { (key, value) -> { spec: GenericExecuteSpec -> spec.bind(key, value) } }
+                  .fold(executeSpecBinderMonoid)
+                  .let { fn -> fn(executeSpec) }
+            }
+            .fetch()
+            .all()
+            .asFlow()
+            .toList()
+      }
 
   suspend fun update(sql: String, params: Map<String, Any> = mapOf()): TryEither<Long> {
     TODO()
