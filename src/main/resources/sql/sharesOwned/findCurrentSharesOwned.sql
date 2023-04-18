e3d3edbcd4a1351e0a920e9623b20528bfb277d8
Craig Miller <craigmiller160@gmail.com>
2023-04-18 18:28:48 -0400

    Working on new sql template

---
+++ src/main/resources/sql/sharesOwned/findCurrentSharesOwned.sql
@@ -0,0 +1,16 @@
+SELECT
+{{^portfolioId}}
+so.portfolio_id,
+{{/portfolioId}}
+so.total_shares, MAX(so.date_range)
+FROM shares_owned so
+WHERE so.user_id = :userId
+{{#portfolioId}}
+AND so.portfolio_id = :portfolioId
+{{/portfolioId}}
+AND so.symbol = :symbol
+GROUP BY
+{{^portfolioId}}
+so.portfolio_id,
+{{/portfolioId}}
+so.total_shares
