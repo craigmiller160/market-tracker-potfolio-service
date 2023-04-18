f57370fce5eb6380cc1a2982a19c812410af23a7
Craig Miller <craigmiller160@gmail.com>
2023-04-18 18:40:01 -0400

    Implemented row mapper

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/CurrentSharesOwnedRowMapper.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/rowmappers/CurrentSharesOwnedRowMapper.kt
@@ -2,4 +2,6 @@ package io.craigmiller160.markettracker.portfolio.domain.rowmappers
 
 import java.math.BigDecimal
 
-val currentSharesOwnedRowMapper: RowMapper<BigDecimal> = { row -> TODO() }
+val currentSharesOwnedRowMapper: RowMapper<BigDecimal> = { row ->
+  row.getRequired("the_sum", BigDecimal::class)
+}
