ccbe2580c9b95779a0a3f0739c81a61b30741018
Craig Miller <craigmiller160@gmail.com>
2023-04-22 10:23:48 -0400

    First attempt at query refactor

--- src/main/resources/sql/sharesOwned/findCurrentSharesOwnedForStock.sql
+++ src/main/resources/sql/sharesOwned/findCurrentSharesOwnedForStock.sql
@@ -2,8 +2,6 @@ SELECT so.total_shares
 FROM shares_owned so
 WHERE so.symbol = :symbol
 AND so.user_id = :userId
-{{#portfolioId}}
-AND so.portfolio_id = :portfolioId
-{{/portfolioId}}
+AND (:portfolioId IS NULL OR so.portfolio_id = :portfolioId)
 ORDER BY so.date_range DESC
 LIMIT 1;
