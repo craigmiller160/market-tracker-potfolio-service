3e3f292bda1521ff43ea93c2a060a65881af47fe
Craig Miller <craigmiller160@gmail.com>
2023-04-23 12:02:49 -0400

    Working more on param builders

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/ParamsBuilder.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/client/ParamsBuilder.kt
@@ -3,16 +3,33 @@ package io.craigmiller160.markettracker.portfolio.domain.client
 fun paramsBuilder(init: ParamsBuilder.() -> Unit): Map<String, Any> =
     ParamsBuilder().apply(init).params
 
-class ParamsBuilder {
-  val params: MutableMap<String, Any> = mutableMapOf()
+// TODO needs tests
+fun batchParamsBuilder(init: BatchParamsBuilder.() -> Unit): List<Any> =
+    BatchParamsBuilder().apply(init).params
 
+sealed class ParamsBuilderSupport {
   inline fun <reified T : Any> nullValue(): NullValue<T> = NullValue(T::class)
+
+  inline fun <reified T : Any> handleNullableValue(value: T?): Any =
+      when (value) {
+        null -> nullValue<T>()
+        else -> value
+      }
+}
+
+class ParamsBuilder : ParamsBuilderSupport() {
+  val params: MutableMap<String, Any> = mutableMapOf()
 
   inline operator fun <reified T : Any> plus(pair: Pair<String, T?>) {
     val (key, value) = pair
-    when (value) {
-      null -> params += key to nullValue<T>()
-      else -> params += key to value
-    }
+    params += key to handleNullableValue(value)
+  }
+}
+
+class BatchParamsBuilder : ParamsBuilderSupport() {
+  var params: MutableList<Any> = mutableListOf()
+
+  inline operator fun <reified T : Any> plus(value: T?) {
+    TODO()
   }
 }
