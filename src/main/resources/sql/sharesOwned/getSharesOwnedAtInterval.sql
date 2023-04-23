cb2261412d147cef92aaea781a1a4b22f13182b8
Craig Miller <craigmiller160@gmail.com>
2023-04-23 15:47:30 -0400

    Heavy refactor of sql query

--- src/main/resources/sql/sharesOwned/getSharesOwnedAtInterval.sql
+++ src/main/resources/sql/sharesOwned/getSharesOwnedAtInterval.sql
@@ -2,21 +2,14 @@ WITH the_dates AS (
     SELECT date_trunc('day', dd)::date AS the_date
     FROM generate_series(:startDate::date, :endDate::date, :interval::interval) dd
 )
-SELECT
-{{#portfolioId}}
-so.portfolio_id, so.total_shares,
-{{/portfolioId}}
-{{^portfolioId}}
-SUM(so.total_shares) AS total_shares,
-{{/portfolioId}}
-td.the_date AS date, so.user_id, so.symbol
+SELECT td.the_date AS date, so.user_id, so.symbol, so.total_shares,
+CASE
+    WHEN :portfolioId IS NOT NULL THEN so.portfolio_id
+END
 FROM the_dates td
 JOIN shares_owned so ON td.the_date <@ so.date_range
 WHERE so.user_id = :userId
 AND so.symbol = :symbol
-{{#portfolioId}}
-AND so.portfolio_id = :portfolioId
-{{/portfolioId}}
-{{^portfolioId}}
-GROUP BY td.the_date, so.user_id, so.symbol
-{{/portfolioId}}
+AND (:portfolioId IS NULL OR so.portfolio_id = :portfolioId)
+
+-- GROUP BY td.the_date, so.user_id, so.symbol
