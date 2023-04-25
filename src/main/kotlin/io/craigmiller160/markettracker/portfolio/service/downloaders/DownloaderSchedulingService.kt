dfaac90514f8020c8e1fa4edfdff3440b4c44647
Craig Miller <craigmiller160@gmail.com>
2023-04-25 09:40:01 -0400

    Fixed that scheduling service was not working

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/DownloaderSchedulingService.kt
@@ -4,11 +4,14 @@ import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import kotlinx.coroutines.runBlocking
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
 
+@Service
 @ConditionalOnProperty(name = ["downloaders.enable-schedule"], havingValue = "true")
 class DownloaderSchedulingService(
     private val downloaderOrchestrationService: DownloaderOrchestrationService
 ) {
+
   @Scheduled(fixedRateString = "#{\${downloaders.interval-rate-hours} * 60 * 60 * 1000}")
   fun downloadAtInterval(): TryEither<Unit> = runBlocking {
     downloaderOrchestrationService.download()
