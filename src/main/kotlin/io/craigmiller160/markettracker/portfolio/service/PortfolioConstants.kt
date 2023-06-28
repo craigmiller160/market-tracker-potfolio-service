271269799d0ef12c2f8f0c81aff9902a6ddd1aa3
Craig Miller <craigmiller160@gmail.com>
2023-06-28 13:57:30 -0400

    Renamed constant

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/PortfolioConstants.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/PortfolioConstants.kt
@@ -5,5 +5,5 @@ import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 
 object PortfolioConstants {
   val COMBINED_PORTFOLIO_ID = TypedId<PortfolioId>("f56f0db2-e77d-43b8-a466-94825ea8db2a")
-  val COMBINED_PORTFOLIO_NAME = "Combined"
+  val COMBINED_PORTFOLIO_NAME = "All Portfolios Combined"
 }
