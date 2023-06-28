f41d7ee77f271af8d772b7ffbe66b321547a78a0
Craig Miller <craigmiller160@gmail.com>
2023-06-27 20:53:37 -0400

    Working on refactoring the logic here

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/PortfolioService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/PortfolioService.kt
@@ -1,18 +1,34 @@
 package io.craigmiller160.markettracker.portfolio.service
 
-import io.craigmiller160.markettracker.portfolio.domain.models.toPortfolioResponse
+import arrow.core.sequence
 import io.craigmiller160.markettracker.portfolio.domain.repository.PortfolioRepository
+import io.craigmiller160.markettracker.portfolio.domain.repository.SharesOwnedRepository
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.craigmiller160.markettracker.portfolio.web.types.PortfolioResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import org.springframework.stereotype.Service
 
 @Service
 class PortfolioService(
     private val portfolioRepository: PortfolioRepository,
+    private val sharesOwnedRepository: SharesOwnedRepository,
     private val authorizationService: AuthorizationService
 ) {
-  suspend fun getPortfolios(): TryEither<List<PortfolioResponse>> =
-      portfolioRepository.findAllForUser(authorizationService.getUserId()).map { portfolios ->
-        portfolios.map { it.toPortfolioResponse() }
-      }
+  suspend fun getPortfolios(): TryEither<List<PortfolioResponse>> {
+    val userId = authorizationService.getUserId()
+    portfolioRepository.findAllForUser(userId).map { portfolios ->
+      portfolios
+          .map {
+            CoroutineScope(Dispatchers.IO).async {
+              sharesOwnedRepository.findUniqueStocksInPortfolio(userId, it.id)
+            }
+          }
+          .awaitAll()
+          .sequence()
+      // TODO need to handle output
+    }
+  }
 }
