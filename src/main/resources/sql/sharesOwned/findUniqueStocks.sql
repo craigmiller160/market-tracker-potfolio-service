95478002a32ff44f4b304ecb4b7b2f9ea3311be3
Craig Miller <craigmiller160@gmail.com>
2023-06-29 15:34:32 -0400

    Got test to pass

--- src/main/resources/sql/sharesOwned/findUniqueStocks.sql
+++ src/main/resources/sql/sharesOwned/findUniqueStocks.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT symbol
 FROM shares_owned
 WHERE user_id = :userId
 AND (:portfolioId IS NULL OR portfolio_id = :portfolioId)
-AND (:startDate IS NULL OR :startDate <@ date_range)
-AND (:endDate IS NULL OR :endDate <@ date_range)
+AND (:startDate IS NULL OR :startDate::date <= lower(date_range))
+AND (:endDate IS NULL OR :endDate::date >= upper(date_range))
 ORDER BY symbol ASC
