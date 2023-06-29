39f6d4d9e4bbfbb7bae1c92b57587a9d79237bcf
Craig Miller <craigmiller160@gmail.com>
2023-06-29 13:01:18 -0400

    Removed casting

--- src/main/resources/sql/sharesOwned/findUniqueStocks.sql
+++ src/main/resources/sql/sharesOwned/findUniqueStocks.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT symbol
 FROM shares_owned
 WHERE user_id = :userId
 AND (:portfolioId IS NULL OR portfolio_id = :portfolioId)
-AND (:startDate IS NULL OR :startDate::date <@ date_range)
-AND (:endDate IS NULL OR :endDate::date <@ date_range)
+AND (:startDate IS NULL OR :startDate <@ date_range)
+AND (:endDate IS NULL OR :endDate <@ date_range)
 ORDER BY symbol ASC
