b56ebbe452c173d74f8811d5cc13ac5bb7cc8bc8
Craig Miller <craigmiller160@gmail.com>
2023-06-29 12:13:02 -0400

    Implementing more param logic

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/PortfolioHandler.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/handlers/PortfolioHandler.kt
@@ -4,6 +4,7 @@ import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.toTypedId
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwnedInterval
+import io.craigmiller160.markettracker.portfolio.extensions.optionalQueryParam
 import io.craigmiller160.markettracker.portfolio.extensions.pathVariable
 import io.craigmiller160.markettracker.portfolio.extensions.requiredQueryParam
 import io.craigmiller160.markettracker.portfolio.service.PortfolioService
@@ -20,7 +21,7 @@ class PortfolioHandler(
     private val sharesOwnedService: SharesOwnedService
 ) {
   suspend fun getPortfolios(request: ServerRequest): ServerResponse =
-      portfolioService.getPortfolios().toResponse()
+      portfolioService.getPortfolios().toResponse() // TODO needs start/end date pair
 
   suspend fun getSharesOwnedForPortfolioStock(request: ServerRequest): ServerResponse {
     val portfolioId = request.portfolioId
@@ -46,6 +47,11 @@ class PortfolioHandler(
 
   private val ServerRequest.stockSymbol: String
     get() = pathVariable("stockSymbol") { it }
+
+  private val ServerRequest.optionalStartDate: LocalDate?
+    get() = optionalQueryParam("startDate", LocalDate::parse)
+  private val ServerRequest.optionalEndDate: LocalDate?
+    get() = optionalQueryParam("endDate", LocalDate::parse)
 
   private val ServerRequest.startDate: LocalDate
     get() = requiredQueryParam("startDate", LocalDate::parse)
