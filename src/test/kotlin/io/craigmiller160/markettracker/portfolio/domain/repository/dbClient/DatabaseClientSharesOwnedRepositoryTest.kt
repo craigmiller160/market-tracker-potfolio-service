4eff01e2e8d082251bdbb6d29fa95d78ea8d9467
Craig Miller <craigmiller160@gmail.com>
2023-07-09 16:03:56 -0400

    More test setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
@@ -1,7 +1,13 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
+import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
+import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
+import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
 import io.craigmiller160.markettracker.portfolio.domain.client.CoroutineDatabaseClient
+import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 
@@ -13,6 +19,19 @@ constructor(
     private val sharesOwnedRepo: DatabaseClientSharesOwnedRepository,
     private val portfolioRepo: DatabaseClientPortfolioRepository
 ) {
+  companion object {
+    private val PORTFOLIO_ID = TypedId<PortfolioId>()
+    private val USER_ID = TypedId<UserId>()
+  }
+
+  @BeforeEach
+  fun setup() {
+    runBlocking {
+      portfolioRepo.createPortfolio(
+          BasePortfolio(id = PORTFOLIO_ID, userId = USER_ID, name = "MyPortfolio"))
+    }
+  }
+
   @Test
   fun `gets the current shares owned for stock in portfolio`() {
     TODO()
