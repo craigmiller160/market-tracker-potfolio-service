d75086749632cbc6cbd5cdef1be4b2ca265babe9
Craig Miller <craigmiller160@gmail.com>
2023-07-09 18:22:00 -0400

    Refactoring query and constants

---
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/domain/DateRangeConstants.kt
@@ -0,0 +1,6 @@
+package io.craigmiller160.markettracker.portfolio.domain
+
+import java.time.LocalDate
+
+val DATE_RANGE_MIN: LocalDate = LocalDate.of(1970, 1, 1)
+val DATE_RANGE_MAX: LocalDate = LocalDate.of(2100, 1, 1)
--- src/main/resources/sql/sharesOwned/findCurrentSharesOwnedForStock.sql
+++ src/main/resources/sql/sharesOwned/findCurrentSharesOwnedForStock.sql
@@ -1,7 +1,6 @@
-SELECT so.total_shares
+SELECT SUM(so.total_shares)
 FROM shares_owned so
 WHERE so.symbol = :symbol
+AND UPPER(date_range) = :maxDate
 AND so.user_id = :userId
-AND (:portfolioId IS NULL OR so.portfolio_id = :portfolioId)
-ORDER BY so.date_range DESC
-LIMIT 1;
+AND (:portfolioId IS NULL OR so.portfolio_id = :portfolioId);
