ee125cb7ac9c7f1c6e9fcfbb6bff1df637e27b01
Craig Miller <craigmiller160@gmail.com>
2023-07-09 17:51:35 -0400

    Refactoring test setup

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
@@ -1,16 +1,10 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
-import arrow.core.getOrElse
 import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
 import io.craigmiller160.markettracker.portfolio.domain.client.CoroutineDatabaseClient
-import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
-import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
-import java.math.BigDecimal
-import java.time.LocalDate
-import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,41 +18,17 @@ constructor(
     private val portfolioRepo: DatabaseClientPortfolioRepository
 ) {
   companion object {
-    private val PORTFOLIO_ID = TypedId<PortfolioId>()
-    private val USER_ID = TypedId<UserId>()
+    private val USER_1_ID = TypedId<UserId>()
+    private val USER_2_ID = TypedId<UserId>()
+    private val PORTFOLIO_1_ID = TypedId<PortfolioId>()
+    private val PORTFOLIO_2_ID = TypedId<PortfolioId>()
+    private val PORTFOLIO_3_ID = TypedId<PortfolioId>()
     private const val STOCK = "VTI"
   }
 
   // TODO add to tests to ensure that other user data isn't pulled out
 
-  @BeforeEach
-  fun setup() {
-    runBlocking {
-      portfolioRepo.createPortfolio(
-          BasePortfolio(id = PORTFOLIO_ID, userId = USER_ID, name = "MyPortfolio"))
-    }
-  }
-
-  private fun createSharesOwned(
-      start: LocalDate,
-      end: LocalDate,
-      totalShares: BigDecimal,
-      portfolioId: TypedId<PortfolioId> = PORTFOLIO_ID
-  ): SharesOwned = runBlocking {
-    sharesOwnedRepo
-        .createAllSharesOwned(
-            listOf(
-                SharesOwned(
-                    id = TypedId(),
-                    portfolioId = portfolioId,
-                    userId = USER_ID,
-                    dateRangeStart = start,
-                    dateRangeEnd = end,
-                    symbol = STOCK,
-                    totalShares = totalShares)))
-        .getOrElse { throw it }
-        .first()
-  }
+  @BeforeEach fun setup() {}
 
   @Test
   fun `gets the current shares owned for stock in portfolio`() {
