2789556ce957d209c94eb0bb503cb79125eba935
Craig Miller <craigmiller160@gmail.com>
2023-07-09 16:07:18 -0400

    Progress

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/domain/repository/dbClient/DatabaseClientSharesOwnedRepositoryTest.kt
@@ -1,11 +1,15 @@
 package io.craigmiller160.markettracker.portfolio.domain.repository.dbClient
 
+import arrow.core.getOrElse
 import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
 import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
 import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
 import io.craigmiller160.markettracker.portfolio.domain.client.CoroutineDatabaseClient
 import io.craigmiller160.markettracker.portfolio.domain.models.BasePortfolio
+import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.testcore.MarketTrackerPortfolioIntegrationTest
+import java.math.BigDecimal
+import java.time.LocalDate
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -22,7 +26,10 @@ constructor(
   companion object {
     private val PORTFOLIO_ID = TypedId<PortfolioId>()
     private val USER_ID = TypedId<UserId>()
+    private const val STOCK = "VTI"
   }
+
+  // TODO add to tests to ensure that other user data isn't pulled out
 
   @BeforeEach
   fun setup() {
@@ -30,6 +37,27 @@ constructor(
       portfolioRepo.createPortfolio(
           BasePortfolio(id = PORTFOLIO_ID, userId = USER_ID, name = "MyPortfolio"))
     }
+  }
+
+  private fun createSharesOwned(
+      start: LocalDate,
+      end: LocalDate,
+      totalShares: BigDecimal,
+      portfolioId: TypedId<PortfolioId> = PORTFOLIO_ID
+  ): SharesOwned = runBlocking {
+    sharesOwnedRepo
+        .createAllSharesOwned(
+            listOf(
+                SharesOwned(
+                    id = TypedId(),
+                    portfolioId = portfolioId,
+                    userId = USER_ID,
+                    dateRangeStart = start,
+                    dateRangeEnd = end,
+                    symbol = STOCK,
+                    totalShares = totalShares)))
+        .getOrElse { throw it }
+        .first()
   }
 
   @Test
