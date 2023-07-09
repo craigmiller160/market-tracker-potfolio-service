30ca3a13b218465aa93f3a7c8ae5319c1fb4a738
Craig Miller <craigmiller160@gmail.com>
2023-07-09 17:58:58 -0400

    Updated test sql

--- src/test/resources/sql/databaseClientSharesOwnedRepositoryTest/all_2.sql
+++ src/test/resources/sql/databaseClientSharesOwnedRepositoryTest/all_2.sql
@@ -1,2 +1,3 @@
 INSERT INTO shares_owned (id, user_id, portfolio_id, date_range, symbol, total_shares)
-VALUES (gen_random_uuid(), :user2Id, :portfolio3Id, '[2023-01-01,2100-01-01)', :stock, 10);
+VALUES (gen_random_uuid(), :user2Id, :portfolio3Id, '[2023-01-01,2100-01-01)', :stock, 10);,
+(gen_random_uuid(), :user1Id, :portfolio1Id, '[2023-01-01,2100-01-01)', 'OTH', 20);
