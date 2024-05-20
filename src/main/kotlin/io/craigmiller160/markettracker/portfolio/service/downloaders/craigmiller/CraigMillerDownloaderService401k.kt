12235c0d78e6a502833cc367892bc22a7cf7dfcd
Craig Miller <craigmiller160@gmail.com>
2024-05-20 10:17:54 -0400

    Fixing compile error

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
@@ -1,10 +1,13 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
+import arrow.core.Either
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.springframework.stereotype.Service
 
 @Service
 class CraigMillerDownloaderService401k : ChildDownloaderService {
-  override suspend fun download(token: String): ChildDownloadServiceResult {
-    TODO("Not yet implemented")
+  override suspend fun download(token: String): ChildDownloadServiceResult = coroutineScope {
+    async { Either.Right(listOf()) }
   }
 }
