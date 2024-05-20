3445c05c7ee5a187de955f74192d68eba076d13a
Craig Miller <craigmiller160@gmail.com>
2024-05-20 13:58:31 -0400

    Progress with logic

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
@@ -6,9 +6,14 @@ import io.craigmiller160.markettracker.portfolio.config.PortfolioConfig
 import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.craigmiller160.markettracker.portfolio.extensions.bindToList
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.DateTimeFormatter
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 
@@ -17,8 +22,13 @@ class CraigMillerDownloaderService401k(
     private val downloaderConfig: CraigMillerDownloaderConfig,
     webClient: WebClient
 ) : AbstractChildDownloaderService(downloaderConfig, webClient) {
+  companion object {}
+
+  private val log = LoggerFactory.getLogger(javaClass)
+
   override suspend fun download(token: String): ChildDownloadServiceResult = coroutineScope {
     async {
+      // TODO download the tradier data
       downloaderConfig.portfolioSpreadsheets401k
           .map { config -> downloadSpreadsheetAsync(config, token) }
           .awaitAll()
@@ -30,6 +40,21 @@ class CraigMillerDownloaderService401k(
   private fun responsesToPortfolios(
       responses: List<Pair<PortfolioConfig, GoogleSpreadsheetValues>>
   ): TryEither<List<PortfolioWithHistory>> {
+    log.debug("Parsing and formatting google spreadsheet responses")
+    println(responses.first().second)
     TODO()
   }
+
+  private fun transformResponse(
+      portfolioName: String,
+      response: GoogleSpreadsheetValues
+  ): TryEither<Any> {
+    response.values.drop(1).map { cols -> cols[0].toDate() to cols[7].toAmount() }
+  }
 }
+
+private val DATE_FORMAT = DateTimeFormatter.ofPattern("MMM yyyy")
+
+private fun String.toDate(): LocalDate = YearMonth.parse(this, DATE_FORMAT).atDay(1)
+
+private fun String.toAmount(): BigDecimal = this.replace(Regex("^\\$"), "").let { BigDecimal(it) }
