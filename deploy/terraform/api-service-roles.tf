577c1bb7fb70e808864f0d21ee2b79ff7b6c99b8
Craig Miller <craigmiller160@gmail.com>
2024-05-19 20:54:30 -0400

    Fixed terraform script

--- deploy/terraform/api-service-roles.tf
+++ deploy/terraform/api-service-roles.tf
@@ -10,20 +10,6 @@ data "keycloak_openid_client" "market_tracker_api_client_prod" {
   client_id = "market-tracker-api"
 }
 
-data "keycloak_role" "market_tracker_api_access_role_dev" {
-  count = var.market_tracker_api_available
-  realm_id = data.keycloak_realm.apps_dev.id
-  client_id = data.keycloak_openid_client.market_tracker_api_client_dev[0].id
-  name = local.access_role_common.name
-}
-
-data "keycloak_role" "market_tracker_api_access_role_prod" {
-  count = var.market_tracker_api_available
-  realm_id = data.keycloak_realm.apps_prod.id
-  client_id = data.keycloak_openid_client.market_tracker_api_client_prod[0].id
-  name = local.access_role_common.name
-}
-
 resource "keycloak_openid_client_service_account_role" "market_tracker_api_service_access_dev" {
   count = var.market_tracker_api_available
   realm_id = data.keycloak_realm.apps_dev.id
