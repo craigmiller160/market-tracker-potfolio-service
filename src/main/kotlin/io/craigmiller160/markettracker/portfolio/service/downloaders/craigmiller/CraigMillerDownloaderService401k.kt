f45dc6253ecff9925f3cf5f0f2f213d53210c018
Craig Miller <craigmiller160@gmail.com>
2024-05-20 13:39:17 -0400

    Stubbing out most important part of implementation

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401k.kt
@@ -1,7 +1,10 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
-import arrow.core.Either
+import arrow.core.flatMap
 import io.craigmiller160.markettracker.portfolio.config.CraigMillerDownloaderConfig
+import io.craigmiller160.markettracker.portfolio.config.PortfolioConfig
+import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
+import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.craigmiller160.markettracker.portfolio.extensions.bindToList
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -20,7 +23,13 @@ class CraigMillerDownloaderService401k(
           .map { config -> downloadSpreadsheetAsync(config, token) }
           .awaitAll()
           .bindToList()
-      Either.Right(listOf())
+          .flatMap { responsesToPortfolios(it) }
     }
+  }
+
+  private fun responsesToPortfolios(
+      responses: List<Pair<PortfolioConfig, GoogleSpreadsheetValues>>
+  ): TryEither<List<PortfolioWithHistory>> {
+    TODO()
   }
 }
