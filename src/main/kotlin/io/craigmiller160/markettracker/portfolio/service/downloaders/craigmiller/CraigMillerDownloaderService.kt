7cc208a56b1497302f26401f28420dee6d9423e5
Craig Miller <craigmiller160@gmail.com>
2024-05-19 20:43:56 -0400

    Refactoring existing downloader service

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService.kt
@@ -8,33 +8,19 @@ import com.nimbusds.jose.JWSHeader
 import com.nimbusds.jose.crypto.RSASSASigner
 import com.nimbusds.jwt.JWTClaimsSet
 import com.nimbusds.jwt.SignedJWT
-import io.craigmiller160.markettracker.portfolio.common.typedid.PortfolioId
-import io.craigmiller160.markettracker.portfolio.common.typedid.TypedId
-import io.craigmiller160.markettracker.portfolio.common.typedid.UserId
 import io.craigmiller160.markettracker.portfolio.config.CraigMillerDownloaderConfig
-import io.craigmiller160.markettracker.portfolio.config.PortfolioConfig
-import io.craigmiller160.markettracker.portfolio.domain.DATE_RANGE_MAX
-import io.craigmiller160.markettracker.portfolio.domain.DATE_RANGE_MIN
 import io.craigmiller160.markettracker.portfolio.domain.models.PortfolioWithHistory
-import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
 import io.craigmiller160.markettracker.portfolio.extensions.TryEither
 import io.craigmiller160.markettracker.portfolio.extensions.awaitBodyResult
-import io.craigmiller160.markettracker.portfolio.extensions.bindToList
 import io.craigmiller160.markettracker.portfolio.extensions.decodePrivateKeyPem
 import io.craigmiller160.markettracker.portfolio.extensions.retrieveSuccess
 import io.craigmiller160.markettracker.portfolio.service.downloaders.DownloaderService
-import java.math.BigDecimal
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.security.KeyFactory
 import java.security.spec.PKCS8EncodedKeySpec
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import kotlinx.collections.immutable.PersistentList
-import kotlinx.collections.immutable.PersistentMap
-import kotlinx.collections.immutable.mutate
-import kotlinx.collections.immutable.persistentListOf
-import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
@@ -42,13 +28,13 @@ import org.springframework.stereotype.Service
 import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.reactive.function.client.WebClient.ResponseSpec
 
 @Service
 class CraigMillerDownloaderService(
     private val downloaderConfig: CraigMillerDownloaderConfig,
     private val webClient: WebClient,
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val downloaderServiceStandard: CraigMillerDownloaderServiceStandard
 ) : DownloaderService {
   companion object {
     const val SPREADSHEET_SCOPE = "https://www.googleapis.com/auth/spreadsheets.readonly"
@@ -66,76 +52,8 @@ class CraigMillerDownloaderService(
 
     return createJwt(serviceAccount)
         .flatMap { jwt -> getAccessToken(serviceAccount, jwt) }
-        .flatMap { token ->
-          downloaderConfig.portfolioSpreadsheetsStandard
-              .map { config -> config.name to getTransactionDataFromSpreadsheet(config, token) }
-              .map { (name, response) ->
-                response.awaitBodyResult<GoogleSpreadsheetValues>().map { name to it }
-              }
-              .bindToList()
-        }
-        .flatMap { responsesToPortfolios(it) }
+        .flatMap { token -> downloaderServiceStandard.download(token) }
         .also { log.info("Completed download of Craig Miller portfolio data") }
-  }
-
-  private fun responsesToPortfolios(
-      responses: List<Pair<String, GoogleSpreadsheetValues>>
-  ): TryEither<List<PortfolioWithHistory>> {
-    log.debug("Parsing and formatting google spreadsheet responses")
-    return responses.map { (name, response) -> transformResponse(name, response) }.bindToList()
-  }
-
-  private fun transformResponse(
-      portfolioName: String,
-      response: GoogleSpreadsheetValues
-  ): TryEither<PortfolioWithHistory> {
-    val portfolioId = TypedId<PortfolioId>()
-    return response.values
-        .drop(1)
-        .map { CraigMillerTransactionRecord.fromRaw(it) }
-        .bindToList()
-        .map { recordsToSharesOwned(portfolioId, it) }
-        .map { ownershipHistory ->
-          PortfolioWithHistory(
-              id = portfolioId,
-              name = portfolioName,
-              userId = downloaderConfig.userId,
-              ownershipHistory = ownershipHistory)
-        }
-  }
-
-  private fun recordsToSharesOwned(
-      portfolioId: TypedId<PortfolioId>,
-      records: List<CraigMillerTransactionRecord>
-  ): List<SharesOwned> =
-      records
-          .asSequence()
-          .filter { RELEVANT_ACTIONS.contains(it.action) }
-          .filter { record -> !SYMBOL_EXCLUSIONS.any { regex -> regex.matches(record.symbol) } }
-          .sortedWith(CraigMillerTransactionRecord.comparator)
-          .map(initialRecord(portfolioId))
-          .fold(
-              OwnershipContext(sharesOwnedMap = persistentMapOf()),
-              ownershipContextCombine(downloaderConfig.userId, portfolioId))
-          .sharesOwnedMap
-          .values
-          .flatten()
-
-  private fun initialRecord(
-      portfolioId: TypedId<PortfolioId>
-  ): (CraigMillerTransactionRecord) -> OwnershipContext = { record ->
-    val sharesOwned =
-        SharesOwned(
-            id = TypedId(),
-            userId = downloaderConfig.userId,
-            portfolioId = portfolioId,
-            dateRangeStart = record.date,
-            dateRangeEnd = DATE_RANGE_MAX,
-            symbol = record.symbol,
-            totalShares = record.shares)
-    OwnershipContext(
-        sharesOwnedMap = persistentMapOf(record.symbol to persistentListOf(sharesOwned)),
-        record = record)
   }
 
   private suspend fun getAccessToken(
@@ -156,20 +74,6 @@ class CraigMillerDownloaderService(
         .retrieveSuccess()
         .awaitBodyResult<GoogleApiAccessToken>()
         .map { it.accessToken }
-  }
-
-  private fun getTransactionDataFromSpreadsheet(
-      config: PortfolioConfig,
-      accessToken: String
-  ): ResponseSpec {
-    log.debug(
-        "Downloading data from spreadsheet. Sheet: ${config.sheetId} Values: ${config.valuesRange}")
-    return webClient
-        .get()
-        .uri(
-            "${downloaderConfig.googleSheetsApiBaseUrl}/spreadsheets/${config.sheetId}/values/${config.valuesRange}")
-        .header("Authorization", "Bearer $accessToken")
-        .retrieveSuccess()
   }
 
   private suspend fun readServiceAccount(): GoogleApiServiceAccount =
@@ -204,55 +108,4 @@ class CraigMillerDownloaderService(
 
         SignedJWT(header, claims).also { it.sign(signer) }.serialize()
       }
-}
-
-private data class OwnershipContext(
-    val sharesOwnedMap: PersistentMap<String, PersistentList<SharesOwned>>,
-    val record: CraigMillerTransactionRecord? = null
-)
-
-private fun ownershipContextCombine(
-    userId: TypedId<UserId>,
-    portfolioId: TypedId<PortfolioId>
-): (OwnershipContext, OwnershipContext) -> OwnershipContext = { ctx1, ctx2 ->
-  val sharesOwnedList = ctx1.sharesOwnedMap[ctx2.record?.symbol] ?: persistentListOf()
-  val lastSharesOwned = sharesOwnedList.lastOrNull()
-  val lastTotalShares = lastSharesOwned?.totalShares ?: BigDecimal("0")
-  val replaceLastSharesOwned =
-      lastSharesOwned?.dateRangeStart == ctx2.record?.date ?: DATE_RANGE_MIN
-
-  val totalShares =
-      when (ctx2.record?.action) {
-        Action.BUY,
-        Action.BONUS -> lastTotalShares + ctx2.record.shares
-        Action.SELL -> lastTotalShares - ctx2.record.shares
-        else -> BigDecimal("0")
-      }
-
-  val newSharesOwned =
-      SharesOwned(
-          id = TypedId(),
-          userId = userId,
-          portfolioId = portfolioId,
-          dateRangeStart = ctx2.record?.date ?: DATE_RANGE_MIN,
-          dateRangeEnd = DATE_RANGE_MAX,
-          symbol = ctx2.record?.symbol ?: "",
-          totalShares = totalShares)
-
-  val newMap =
-      ctx1.sharesOwnedMap.mutate { map ->
-        map[ctx2.record?.symbol ?: ""] =
-            sharesOwnedList.mutate { list ->
-              if (replaceLastSharesOwned) {
-                list[list.size - 1] = newSharesOwned
-              } else {
-                lastSharesOwned?.let { lastSharesOwnedReal ->
-                  list[list.size - 1] =
-                      lastSharesOwnedReal.copy(dateRangeEnd = ctx2.record?.date ?: DATE_RANGE_MAX)
-                }
-                list += newSharesOwned
-              }
-            }
-      }
-  OwnershipContext(sharesOwnedMap = newMap)
 }
