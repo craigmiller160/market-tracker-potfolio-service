9cfaa554ff500dfc87496e4a6ab08a2b33f5ca32
Craig Miller <craigmiller160@gmail.com>
2024-05-21 10:58:54 -0400

    Updated dto

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/types/keycloak/TokenResponse.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/web/types/keycloak/TokenResponse.kt
@@ -5,10 +5,5 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class TokenResponse(
     @field:JsonProperty("access_token") val accessToken: String,
     @field:JsonProperty("expires_in") val expiresIn: Int,
-    @field:JsonProperty("refresh_expires_in") val refreshExpiresIn: Int,
-    @field:JsonProperty("refresh_token") val refreshToken: String,
-    @field:JsonProperty("token_type") val tokenType: String,
-    @field:JsonProperty("not-before-policy") val notBeforePolicy: Int,
-    @field:JsonProperty("session_state") val sessionState: String,
-    val scope: String
+    @field:JsonProperty("token_type") val tokenType: String
 )
