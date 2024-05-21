832ba9a170963d38e6c0fa4c53e4060b02737661
Craig Miller <craigmiller160@gmail.com>
2024-05-21 15:33:40 -0400

    More work on extensions

--- src/main/kotlin/io/craigmiller160/markettracker/portfolio/extensions/LocalDateExtensions.kt
+++ src/main/kotlin/io/craigmiller160/markettracker/portfolio/extensions/LocalDateExtensions.kt
@@ -5,3 +5,8 @@ import java.time.LocalDate
 fun LocalDate.isBetween(startDate: LocalDate, endDate: LocalDate): Boolean =
     (this.isAfter(startDate) || this.isEqual(startDate)) &&
         (this.isBefore(endDate) || this.isEqual(endDate))
+
+fun LocalDate.isAfterOrEqual(other: LocalDate): Boolean = this.isAfter(other) || this.isEqual(other)
+
+fun LocalDate.isBeforeOrEqual(other: LocalDate): Boolean =
+    this.isBefore(other) || this.isEqual(other)
