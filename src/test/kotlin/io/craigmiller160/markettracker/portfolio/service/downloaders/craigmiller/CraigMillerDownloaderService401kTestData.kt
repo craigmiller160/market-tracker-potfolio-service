0600179b3036a3767077d0f9c2d318581a2d6630
Craig Miller <craigmiller160@gmail.com>
2024-05-24 07:03:00 -0400

    Preparing for dynamic expected data generation

--- src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401kTestData.kt
+++ src/test/kotlin/io/craigmiller160/markettracker/portfolio/service/downloaders/craigmiller/CraigMillerDownloaderService401kTestData.kt
@@ -1,5 +1,35 @@
 package io.craigmiller160.markettracker.portfolio.service.downloaders.craigmiller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.craigmiller160.markettracker.portfolio.domain.models.SharesOwned
+import io.craigmiller160.markettracker.portfolio.testutils.DataLoader
+import io.craigmiller160.markettracker.portfolio.web.types.tradier.TradierHistory
+import java.math.BigDecimal
+import java.time.LocalDate
 
-val TEST_DATA_401K: List<SharesOwned> = createTestData {}
+// 12/2022 is when VXUS starts
+
+private val objectMapper = jacksonObjectMapper()
+
+private val data401k =
+    DataLoader.load("data/craigmiller/Data401k.json").let {
+      objectMapper.readValue(it, GoogleSpreadsheetValues::class.java)
+    }
+private val vtiHistory =
+    DataLoader.load("data/craigmiller/TradierHistoryFor401k_VTI.json").let {
+      objectMapper.readValue(it, TradierHistory::class.java)
+    }
+private val vxusHistory =
+    DataLoader.load("data/craigmiller/TradierHistoryFor401k_VXUS.json").let {
+      objectMapper.readValue(it, TradierHistory::class.java)
+    }
+
+val TEST_DATA_401K: List<SharesOwned> = createTestData {
+  shares {
+    start = LocalDate.of(2022, 2, 1)
+    end = LocalDate.of(2022, 3, 1)
+    symbol = "VTI"
+    amount = BigDecimal("228.0236")
+  }
+}
